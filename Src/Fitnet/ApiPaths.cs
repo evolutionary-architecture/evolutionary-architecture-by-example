c57c1a9f7eca7b93bd480a20c2642601cddec52d
Maciej Jedrzejewski <mac.jedrzejewski@gmail.com>
2023-03-22 12:43:21 +0100

    feat: add new api path

---
+++ Src/Fitnet/ApiPaths.cs
@@ -0,0 +1,9 @@
+namespace SuperSimpleArchitecture.Fitnet;
+
+public static class ApiPaths
+{
+    private const string Api = "api";
+    
+    public const string Passes = $"{Api}/passes";
+    public const string Contracts = $"{Api}/contracts";
+}
--- Src/Fitnet/Passes/ApiPaths.cs
+++
@@ -1,6 +0,0 @@
-namespace SuperSimpleArchitecture.Fitnet.Passes;
-
-public static class ApiPaths
-{
-    public const string Passes = "api/passes";
-}
