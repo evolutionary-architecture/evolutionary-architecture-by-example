af958516b339d1db03de8a9b05fce61d4bf36c0f
Maciej Jedrzejewski <mac.jedrzejewski@gmail.com>
2023-04-02 16:20:33 +0200

    feat: add business rule for checking customer age

---
+++ Src/Fitnet/Contracts/PrepareContract/BusinessRules/ContractCanBePreparedOnlyForAdultRule.cs
@@ -0,0 +1,16 @@
+namespace SuperSimpleArchitecture.Fitnet.Contracts.PrepareContract.BusinessRules;
+
+using Shared.BusinessRulesEngine;
+
+internal class ContractCanBePreparedOnlyForAdultRule : IBusinessRule
+{
+    private readonly int _age;
+    internal ContractCanBePreparedOnlyForAdultRule(int age)
+    {
+        _age = age;
+    }
+
+    public bool IsMet() => _age >= 18;
+
+    public string Error => "Contract can not be prepared for a person who is not adult";
+}
