f83087e0b20fbbd822ff401037c1a3a51457c68c
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2023-04-04 19:50:27 +0200

    fix: add file verify to tests

--- Src/Fitnet.IntegrationTests/Reports/GenerateNewPassesPerMonthReport/GenerateNewPassesPerMonthReportTests.cs
+++ Src/Fitnet.IntegrationTests/Reports/GenerateNewPassesPerMonthReport/GenerateNewPassesPerMonthReportTests.cs
@@ -1,8 +1,12 @@
 namespace SuperSimpleArchitecture.Fitnet.IntegrationTests.Reports.GenerateNewPassesPerMonthReport;
 
+using System.Runtime.CompilerServices;
 using Fitnet.Reports;
 using Common.TestEngine;
 using Common.TestEngine.Configuration;
+using Fitnet.Passes;
+using Fitnet.Passes.RegisterPass;
+using Passes.RegisterPass;
 
 [UsesVerify]
 public sealed class GenerateNewPassesPerMonthReportTests : IClassFixture<WebApplicationFactory<Program>>, IClassFixture<DatabaseContainer>
@@ -14,12 +18,17 @@ public sealed class GenerateNewPassesPerMonthReportTests : IClassFixture<WebAppl
         _applicationHttpClient = applicationInMemoryFactory
             .WithContainerDatabaseConfigured(database.ConnectionString!)
             .CreateClient();
+    
+    [ModuleInitializer]
+    internal static void Initialize() =>
+        VerifyAspose.Initialize();
 
     [Fact]
     public async Task Given_valid_generate_new_report_request_Then_should_return_correct_pdf_report()
     {
         // Arrange
-        
+        await RegisterPass();
+
         // Act
         var getReportResult = await _applicationHttpClient.GetAsync(ReportsApiPaths.GenerateNewReport);
 
@@ -27,5 +36,15 @@ public sealed class GenerateNewPassesPerMonthReportTests : IClassFixture<WebAppl
         getReportResult.Content.Headers.ContentType!.MediaType.Should().Be("application/pdf");
         var fileBytes = await getReportResult.Content.ReadAsStreamAsync();
         fileBytes.Should().NotBeNull();
+         await Verify(fileBytes, "pdf");  
+    }
+    
+    private async Task<Guid> RegisterPass()
+    {
+        RegisterPassRequest registerPassRequest = new RegisterPassRequestFaker();
+        var registerPassResponse = await _applicationHttpClient.PostAsJsonAsync(PassesApiPaths.Register, registerPassRequest);
+        var registeredPassId = await registerPassResponse.Content.ReadFromJsonAsync<Guid>();
+
+        return registeredPassId;
     }
 }
