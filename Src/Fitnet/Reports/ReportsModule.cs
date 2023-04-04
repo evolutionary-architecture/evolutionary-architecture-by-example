3d7516bc22d8e731bae3a853b090e52a34bc1b3d
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2023-04-04 19:50:57 +0200

    fix: register interfaces in dependecy injection

--- Src/Fitnet/Reports/ReportsModule.cs
+++ Src/Fitnet/Reports/ReportsModule.cs
@@ -1,9 +1,18 @@
 namespace SuperSimpleArchitecture.Fitnet.Reports;
 
+using GenerateNewPassesPerMonthReport.DataRetriver;
+using GenerateNewPassesPerMonthReport.ReportGenerator;
+
 internal static class ReportsModule
 {
-    internal static IServiceCollection AddReports(this IServiceCollection services) => 
-        services;
+    internal static IServiceCollection AddReports(this IServiceCollection services)
+    {
+        services.AddSingleton<IDatabaseConnectionFactory, DatabaseConnectionFactory>();
+        services.AddSingleton<INewPassesPerMonthReportDataRetriever, NewPassesPerMonthReportDataRetriever>();
+        services.AddSingleton<INewPassesPerMonthReportDataPdfReportGenerator, NewPassesPerMonthReportDataPdfReportGenerator>();
+
+        return services;
+    } 
 
     internal static IApplicationBuilder UseReports(this IApplicationBuilder applicationBuilder) => 
         applicationBuilder;
