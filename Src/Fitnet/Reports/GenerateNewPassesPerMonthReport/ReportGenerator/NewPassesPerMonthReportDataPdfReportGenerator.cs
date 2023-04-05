570381ace6e452336cdee65538fcfac18770e454
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2023-04-05 20:53:31 +0200

    refactor: change implementation to use quest pdf

--- Src/Fitnet/Reports/GenerateNewPassesPerMonthReport/ReportGenerator/NewPassesPerMonthReportDataPdfReportGenerator.cs
+++ Src/Fitnet/Reports/GenerateNewPassesPerMonthReport/ReportGenerator/NewPassesPerMonthReportDataPdfReportGenerator.cs
@@ -1,42 +1,66 @@
 namespace SuperSimpleArchitecture.Fitnet.Reports.GenerateNewPassesPerMonthReport.ReportGenerator;
 
 using Dtos;
-using iText.Kernel.Pdf;
-using iText.Layout;
-using iText.Layout.Element;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
 
 internal sealed class NewPassesPerMonthReportDataPdfReportGenerator : INewPassesPerMonthReportDataPdfReportGenerator
 {
-    const int columnCount = 4;
-
-    public async Task<byte[]> GeneratePdfReportAsync(string name, IReadOnlyCollection<NewPassesPerMonthDto> report, CancellationToken cancellationToken)
+    public Task<byte[]> GeneratePdfReportAsync(string name, IReadOnlyCollection<NewPassesPerMonthDto> report, CancellationToken cancellationToken)
     {
-        await using var file = new MemoryStream();
-        await using var writer = new PdfWriter(file);
-        var pdf = new PdfDocument(writer);
-        using var document = new Document(pdf);
-        var table = new Table(columnCount);
-        AddTableHeader(table);
-        AddTableData(table, report);
-        document.Add(table);
-
-        var bytes = file.ToArray()!;
-
-        return bytes;
-    }
-
-    private static void AddTableHeader(Table table)
-    {
-        table.AddHeaderCell(nameof(NewPassesPerMonthDto.RegisteredPasses));
-        table.AddHeaderCell(nameof(NewPassesPerMonthDto.Month));
-    }
-
-    private static void AddTableData(Table table, IEnumerable<NewPassesPerMonthDto> passes)
-    {
-        foreach (var pass in passes)
+        var document = Document.Create(container =>
         {
-            table.AddCell(pass.RegisteredPasses.ToString());
-            table.AddCell(pass.Month);
-        }
+            container.Page(page =>
+            {
+                page.Size(PageSizes.A4);
+                page.Margin(20);
+
+                page.Content().Column(columnDescriptor =>
+                {
+                    columnDescriptor.Item()
+                        .Element(x => x.PaddingVertical(10))
+                        .Text("Registered passes per month 2023 Report")
+                        .Bold()
+                        .FontSize(13);
+
+                    columnDescriptor.Item().Table(tableDescriptor =>
+                    {
+                        IContainer DefaultCellStyle(IContainer container, string backgroundColor)
+                        {
+                            return container
+                                .Border(1)
+                                .BorderColor(Colors.Grey.Lighten1)
+                                .Background(backgroundColor)
+                                .PaddingVertical(5)
+                                .PaddingHorizontal(10)
+                                .AlignCenter()
+                                .AlignMiddle();
+                        }
+                    
+                        tableDescriptor.ColumnsDefinition(columns =>
+                        {
+                            columns.ConstantColumn(150);
+                            columns.ConstantColumn(150);
+                        });
+                        tableDescriptor.Header(y =>
+                        {
+                            y.Cell().Element(HeaderStyle).Text("Month").Bold();
+                            y.Cell().Element(HeaderStyle).Text("Registered Passes").Bold();
+                        });
+
+                        foreach (var rNewPassesPerMonthDto in report)
+                        {
+                            tableDescriptor.Cell().Element(CellStyle).Text(rNewPassesPerMonthDto.Month);
+                            tableDescriptor.Cell().Element(CellStyle).Text(rNewPassesPerMonthDto.RegisteredPasses.ToString());
+                        }
+                    
+                        IContainer CellStyle(IContainer container) => DefaultCellStyle(container, Colors.White); 
+                        IContainer HeaderStyle(IContainer container) => DefaultCellStyle(container, Colors.Grey.Lighten3);                    });
+                });
+            });
+        });
+
+        return Task.FromResult(document.GeneratePdf());
     }
 }
