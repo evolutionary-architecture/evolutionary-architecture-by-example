7184fb0d447f5ed3ccc91a6a6a282837ff8acaa8
Maciej Jedrzejewski <mac.jedrzejewski@gmail.com>
2023-04-07 16:39:53 +0200

    feat: add business rule for contract signature date check

---
+++ Src/Fitnet.UnitTests/Contracts/SignContract/BusinessRules/ContractCanOnlyBeSignedWithin30DaysFromPreparationTests.cs
@@ -0,0 +1,53 @@
+using SuperSimpleArchitecture.Fitnet.Contracts.SignContract.BusinessRules;
+using SuperSimpleArchitecture.Fitnet.Shared.BusinessRulesEngine;
+
+namespace SuperSimpleArchitecture.Fitnet.UnitTests.Contracts.SignContract.BusinessRules;
+
+public sealed class ContractCanOnlyBeSignedWithin30DaysFromPreparationTests
+{
+    [Fact]
+    public void Given_signed_at_date_which_is_more_than_30_days_from_prepared_at_date_Then_validation_should_throw()
+    {
+        // Arrange
+        
+        // Act
+        var act = () =>
+            BusinessRuleValidator.Validate(
+                new ContractCanOnlyBeSignedWithin30DaysFromPreparation(DateTimeOffset.Now,
+                    DateTimeOffset.Now.AddDays(31)));
+        
+        // Assert
+        act.Should().Throw<BusinessRuleValidationException>().WithMessage(
+            "Contract can not be signed because more than 30 days have passed from the contract preparation");
+    }
+    
+    [Fact]
+    public void Given_signed_at_date_which_is_30_days_from_prepared_at_date_Then_validation_should_pass()
+    {
+        // Arrange
+
+        // Act
+        var act = () =>
+            BusinessRuleValidator.Validate(
+                new ContractCanOnlyBeSignedWithin30DaysFromPreparation(DateTimeOffset.Now,
+                    DateTimeOffset.Now.AddDays(30)));;
+        
+        // Assert
+        act.Should().NotThrow<BusinessRuleValidationException>();
+    }
+    
+    [Fact]
+    public void Given_signed_at_date_which_is_less_than_30_days_from_prepared_at_date_Then_validation_should_pass()
+    {
+        // Arrange
+
+        // Act
+        var act = () =>
+            BusinessRuleValidator.Validate(
+                new ContractCanOnlyBeSignedWithin30DaysFromPreparation(DateTimeOffset.Now,
+                    DateTimeOffset.Now.AddDays(29)));;
+        
+        // Assert
+        act.Should().NotThrow<BusinessRuleValidationException>();
+    }
+}
