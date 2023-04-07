a4557b2310d978300449819807102301c048ca00
Maciej Jedrzejewski <mac.jedrzejewski@gmail.com>
2023-04-07 14:56:41 +0200

    refactor: extract common path for contracts API to separate const

--- Src/Fitnet/Contracts/ContractsApiPaths.cs
+++ Src/Fitnet/Contracts/ContractsApiPaths.cs
@@ -2,6 +2,8 @@ namespace SuperSimpleArchitecture.Fitnet.Contracts;
 
 internal static class ContractsApiPaths
 {
-    internal const string Prepare = $"{ApiPaths.Root}/contracts";
-    internal const string Sign = $"{ApiPaths.Root}/contracts/{{id}}";
+    private const string ContractsRootApi = $"{ApiPaths.Root}/contracts";
+    
+    internal const string Prepare = ContractsRootApi;
+    internal const string Sign = $"{ContractsRootApi}/{{id}}";
 }
