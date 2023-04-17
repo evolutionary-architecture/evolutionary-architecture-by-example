0d3fd5adbe295d52ef0ecb55b74f5549aed5ac6a
Maciej Jedrzejewski <mac.jedrzejewski@gmail.com>
2023-04-17 12:35:55 +0200

    refactor: remove configurable prepared date

--- Src/Fitnet.IntegrationTests/Contracts/PrepareContract/PrepareContractRequestFaker.cs
+++ Src/Fitnet.IntegrationTests/Contracts/PrepareContract/PrepareContractRequestFaker.cs
@@ -4,14 +4,13 @@ using SuperSimpleArchitecture.Fitnet.Contracts.PrepareContract;
 
 internal sealed class PrepareContractRequestFaker : Faker<PrepareContractRequest>
 {
-    public PrepareContractRequestFaker(int minAge, int maxAge, int minHeight, int maxHeight,
-        DateTimeOffset? preparedAt = null)
+    public PrepareContractRequestFaker(int minAge, int maxAge, int minHeight, int maxHeight)
     {
         CustomInstantiator(faker =>
             new PrepareContractRequest(
                 faker.Random.Number(minAge, maxAge),
                 faker.Random.Number(minHeight, maxHeight),
-                preparedAt?.ToUniversalTime() ?? faker.Date.RecentOffset().ToUniversalTime()
+                faker.Date.RecentOffset().ToUniversalTime()
             )
         );
     }
