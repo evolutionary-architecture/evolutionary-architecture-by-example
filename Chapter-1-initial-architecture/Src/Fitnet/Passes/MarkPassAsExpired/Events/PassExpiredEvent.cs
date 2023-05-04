28c4e6024f6535b6186d5c5bd39df0d732ab553e
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2023-05-04 20:43:13 +0200

    refactor: make pass expired event interal

--- Chapter-1-initial-architecture/Src/Fitnet/Passes/MarkPassAsExpired/Events/PassExpiredEvent.cs
+++ Chapter-1-initial-architecture/Src/Fitnet/Passes/MarkPassAsExpired/Events/PassExpiredEvent.cs
@@ -2,7 +2,7 @@ namespace EvolutionaryArchitecture.Fitnet.Passes.MarkPassAsExpired.Events;
 
 using EvolutionaryArchitecture.Fitnet.Shared.Events;
 
-public record PassExpiredEvent(Guid Id, Guid PassId,Guid CustomerId, DateTimeOffset OccurredDateTime) : IIntegrationEvent
+internal record PassExpiredEvent(Guid Id, Guid PassId, Guid CustomerId, DateTimeOffset OccurredDateTime) : IIntegrationEvent
 {
     internal static PassExpiredEvent Create(Guid passId, Guid customerId) => new(Guid.NewGuid(), passId, customerId, DateTimeOffset.Now);
 }
