2884f9f2adf22179960eac7320d2a0055c98afa9
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2023-05-20 23:41:17 +0200

    refactor: move contracts core to dedicated project

---
+++ Chapter-2-modules-separation/Src/Fitnet.Contracts.Core/Contract.cs
@@ -0,0 +1,36 @@
+namespace EvolutionaryArchitecture.Fitnet.Contracts.Core;
+
+using Common.BussinessRules;
+using PrepareContract.BusinessRules;
+using SignContract.BusinessRules;
+
+public sealed class Contract
+{
+    public Guid Id { get; init; }
+    
+    public DateTimeOffset PreparedAt { get; init; }
+    
+    public DateTimeOffset SignedAt { get; private set; }
+
+    private Contract(Guid id, DateTimeOffset preparedAt)
+    {
+        Id = id;
+        PreparedAt = preparedAt;
+    }
+
+    public static Contract Prepare(int customerAge, int customerHeight, DateTimeOffset preparedAt)
+    {
+        BusinessRuleValidator.Validate(new ContractCanBePreparedOnlyForAdultRule(customerAge));
+        BusinessRuleValidator.Validate(new CustomerMustBeSmallerThanMaximumHeightLimitRule(customerHeight));
+        
+        return new(Guid.NewGuid(), preparedAt);
+    }
+
+    public void Sign(DateTimeOffset signedAt)
+    {
+        BusinessRuleValidator.Validate(
+            new ContractCanOnlyBeSignedWithin30DaysFromPreparation(PreparedAt, signedAt));
+
+        SignedAt = signedAt;
+    }
+}
---
+++ Chapter-2-modules-separation/Src/Fitnet.Contracts.Core/PrepareContract/BusinessRules/ContractCanBePreparedOnlyForAdultRule.cs
@@ -0,0 +1,14 @@
+namespace EvolutionaryArchitecture.Fitnet.Contracts.Core.PrepareContract.BusinessRules;
+
+using Common.BussinessRules;
+
+internal sealed class ContractCanBePreparedOnlyForAdultRule : IBusinessRule
+{
+    private readonly int _age;
+    
+    internal ContractCanBePreparedOnlyForAdultRule(int age) => _age = age;
+
+    public bool IsMet() => _age >= 18;
+
+    public string Error => "Contract can not be prepared for a person who is not adult";
+}
