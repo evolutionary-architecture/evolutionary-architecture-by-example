4a8104dea4dc58709539a9d7eba40b5a36b922d0
Maciej Jedrzejewski <mac.jedrzejewski@gmail.com>
2023-05-24 16:11:16 +0200

    feat: add mediator module

---
+++ Chapter-2-modules-separation/Src/Fitnet.Common.Infrastructure/Mediator/MediatorModule.cs
@@ -0,0 +1,15 @@
+namespace EvolutionaryArchitecture.Fitnet.Common.Infrastructure.Mediator;
+
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+public static class MediatorModule
+{
+    public static IServiceCollection AddMediator(this IServiceCollection services, Assembly assembly)
+    {
+        services.AddMediatR(configuration => configuration.RegisterServicesFromAssembly(assembly));
+        
+        return services;
+    }
+    
+}
