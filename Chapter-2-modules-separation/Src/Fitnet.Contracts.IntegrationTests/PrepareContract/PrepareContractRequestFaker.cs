cd878721948b55e1b3a2302cf9942dea6f141bde
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2023-06-01 21:06:46 +0200

    tests: add given contract preparation request When contract for customer was prepared earlier and was not signed yet test case

--- Chapter-2-modules-separation/Src/Fitnet.Contracts.IntegrationTests/PrepareContract/PrepareContractRequestFaker.cs
+++ Chapter-2-modules-separation/Src/Fitnet.Contracts.IntegrationTests/PrepareContract/PrepareContractRequestFaker.cs
@@ -4,10 +4,11 @@ using Api.Prepare;
 
 internal sealed class PrepareContractRequestFaker : Faker<PrepareContractRequest>
 {
-    internal PrepareContractRequestFaker(int minAge, int maxAge, int minHeight, int maxHeight)
+    internal PrepareContractRequestFaker(int minAge, int maxAge, int minHeight, int maxHeight, Guid? customerId = null)
     {
         CustomInstantiator(faker =>
             new PrepareContractRequest(
+                customerId ?? faker.Random.Guid(),
                 faker.Random.Number(minAge, maxAge),
                 faker.Random.Number(minHeight, maxHeight),
                 faker.Date.RecentOffset().ToUniversalTime()
