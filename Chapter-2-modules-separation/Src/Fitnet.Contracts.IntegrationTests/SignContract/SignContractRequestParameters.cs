5288b0bddd93f4e47be92d41ae61db3919b9e6e6
Maciej Jedrzejewski <mac.jedrzejewski@gmail.com>
2023-06-02 08:16:59 +0200

    refactor: flatten structure of contract integration tests

--- Chapter-2-modules-separation/Src/Fitnet.Contracts.IntegrationTests/SignContract/SignContractRequestParameters.cs
+++ Chapter-2-modules-separation/Src/Fitnet.Contracts.IntegrationTests/SignContract/SignContractRequestParameters.cs
@@ -1,4 +1,4 @@
-namespace EvolutionaryArchitecture.Fitnet.Contracts.IntegrationTests.Contracts.SignContract;
+namespace EvolutionaryArchitecture.Fitnet.Contracts.IntegrationTests.SignContract;
 
 using Api;
 
