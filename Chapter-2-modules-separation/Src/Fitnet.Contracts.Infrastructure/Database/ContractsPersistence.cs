e9d02012e8ca788faf4753221d331c5f99dc8bf2
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2023-06-04 11:19:52 +0200

    refactor: remove unessery class

--- Chapter-2-modules-separation/Src/Fitnet.Contracts.Infrastructure/Database/ContractsPersistence.cs
+++ Chapter-2-modules-separation/Src/Fitnet.Contracts.Infrastructure/Database/ContractsPersistence.cs
@@ -2,18 +2,6 @@ namespace EvolutionaryArchitecture.Fitnet.Contracts.Infrastructure.Database;
 
 using Core;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
-
-public class BloggingContextFactory : IDesignTimeDbContextFactory<ContractsPersistence>
-{
-    public ContractsPersistence CreateDbContext(string[] args)
-    {
-        var optionsBuilder = new DbContextOptionsBuilder<ContractsPersistence>();
-        optionsBuilder.UseNpgsql("Host=localhost:5432;Database=fitnet;Username=postgres;Password=mysecretpassword");
-
-        return new ContractsPersistence(optionsBuilder.Options);
-    }
-}
 
 public sealed class ContractsPersistence : DbContext
 {
