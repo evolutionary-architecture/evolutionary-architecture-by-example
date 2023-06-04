6e13a3f21be6cae96d913faa79e4dd3e60a3d3c1
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2023-06-04 11:17:37 +0200

    refactor: change exception message

--- Chapter-2-modules-separation/Src/Fitnet.Contracts.Application/Commands/Prepare/Exceptions/CustomerHasUnsignedContractException.cs
+++ Chapter-2-modules-separation/Src/Fitnet.Contracts.Application/Commands/Prepare/Exceptions/CustomerHasUnsignedContractException.cs
@@ -5,7 +5,7 @@ using EvolutionaryArchitecture.Fitnet.Common.Core.BusinessRules;
 internal sealed class CustomerHasUnsignedContractException : BusinessRuleValidationException
 {
     internal CustomerHasUnsignedContractException(Guid customerId, Guid contractId, DateTimeOffset preparedAt) : 
-        base($"Customer '{customerId}' has not signed contract '{contractId}' prepared at '{preparedAt:F}'. New contract cannot be prepared for this customer.")
+        base($"Customer '{customerId}' has unsigned contract '{contractId}' prepared at '{preparedAt:F}'. New contract cannot be prepared for this customer.")
     {
     }
 }
