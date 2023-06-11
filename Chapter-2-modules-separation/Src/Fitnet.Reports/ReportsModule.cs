f1336308ac04a66aee30c0d0d1a864e6e780e167
Maciej Jedrzejewski <mac.jedrzejewski@gmail.com>
2023-06-11 19:00:18 +0200

    feat: add registration of reports

--- Chapter-2-modules-separation/Src/Fitnet.Reports/ReportsModule.cs
+++ Chapter-2-modules-separation/Src/Fitnet.Reports/ReportsModule.cs
@@ -2,19 +2,30 @@ namespace EvolutionaryArchitecture.Fitnet.Reports;
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Common.Infrastructure.Modules;
 using DataAccess;
 using GenerateNewPassesRegistrationsPerMonthReport;
 
 public static class ReportsModule
 {
-    public static IServiceCollection AddReports(this IServiceCollection services)
+    public static void RegisterReports(this WebApplication app, string module)
     {
+        if (!app.IsModuleEnabled(module)) return;
+
+        app.UseReports();
+        app.MapReports();
+    }
+    
+    public static IServiceCollection AddReports(this IServiceCollection services, string module)
+    {
+        if (!services.IsModuleEnabled(module)) return services;
+        
         services.AddDataAccess();
         services.AddNewPassesRegistrationsPerMonthReport();
 
         return services;
-    } 
+    }
 
-    public static IApplicationBuilder UseReports(this IApplicationBuilder applicationBuilder) => 
+    private static IApplicationBuilder UseReports(this IApplicationBuilder applicationBuilder) => 
         applicationBuilder;
 }
