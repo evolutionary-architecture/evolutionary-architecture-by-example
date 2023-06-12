247b1a65ce8f7d3bdb7d192f15de48127b678848
Maciej Jedrzejewski <mac.jedrzejewski@gmail.com>
2023-06-12 14:12:30 +0200

    fix: remove not needed registration

--- Chapter-2-modules-separation/Src/Fitnet.Reports/GenerateNewPassesRegistrationsPerMonthReport/GenerateNewPassesPerMonthReportModule.cs
+++ Chapter-2-modules-separation/Src/Fitnet.Reports/GenerateNewPassesRegistrationsPerMonthReport/GenerateNewPassesPerMonthReportModule.cs
@@ -2,15 +2,13 @@ namespace EvolutionaryArchitecture.Fitnet.Reports.GenerateNewPassesRegistrations
 
 using Microsoft.Extensions.DependencyInjection;
 using DataRetriever;
-using DataAccess;
 
 internal static class GenerateNewPassesPerMonthReportModule
 {
     internal static IServiceCollection AddNewPassesRegistrationsPerMonthReport(this IServiceCollection services)
     {
         services.AddSingleton<INewPassesRegistrationPerMonthReportDataRetriever, NewPassesRegistrationPerMonthReportDataRetriever>();
-        services.AddSingleton<IDatabaseConnectionFactory, DatabaseConnectionFactory>();
-
+        
         return services;
     }
 }
