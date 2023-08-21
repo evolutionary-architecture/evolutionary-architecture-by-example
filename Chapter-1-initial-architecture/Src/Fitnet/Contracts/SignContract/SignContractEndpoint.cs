c9663323379900015a46c32d19a7cd71cae58695
Maciej Jedrzejewski <mac.jedrzejewski@gmail.com>
2023-08-21 10:56:24 +0200

    feat: add swagger description for sign contract endpoint in chapter 1

--- Chapter-1-initial-architecture/Src/Fitnet/Contracts/SignContract/SignContractEndpoint.cs
+++ Chapter-1-initial-architecture/Src/Fitnet/Contracts/SignContract/SignContractEndpoint.cs
@@ -10,20 +10,32 @@ internal static class SignContractEndpoint
     internal static void MapSignContract(this IEndpointRouteBuilder app)
     {
         app.MapPatch(ContractsApiPaths.Sign, async (Guid id, SignContractRequest request,
-            ContractsPersistence persistence, IEventBus bus, ISystemClock systemClock, CancellationToken cancellationToken) =>
-        {
-            var contract =
-                await persistence.Contracts.FindAsync(new object?[] { id }, cancellationToken);
-            if (contract is null)
-                return Results.NotFound();
+                ContractsPersistence persistence, IEventBus bus, ISystemClock systemClock,
+                CancellationToken cancellationToken) =>
+            {
+                var contract =
+                    await persistence.Contracts.FindAsync(new object?[] { id }, cancellationToken);
+                if (contract is null)
+                    return Results.NotFound();
 
-            contract.Sign(request.SignedAt, systemClock.Now);
-            await persistence.SaveChangesAsync(cancellationToken);
-            
-            var @event = ContractSignedEvent.Create(contract.Id, contract.CustomerId, contract.SignedAt!.Value, contract.ExpiringAt!.Value);
-            await bus.PublishAsync(@event, cancellationToken);
-            
-            return Results.NoContent();
-        });
+                contract.Sign(request.SignedAt, systemClock.Now);
+                await persistence.SaveChangesAsync(cancellationToken);
+
+                var @event = ContractSignedEvent.Create(contract.Id, contract.CustomerId, contract.SignedAt!.Value,
+                    contract.ExpiringAt!.Value);
+                await bus.PublishAsync(@event, cancellationToken);
+
+                return Results.NoContent();
+            })
+            .WithOpenApi(operation => new(operation)
+            {
+                Summary = "Signs prepared contract",
+                Description =
+                    "This endpoint is used to sign prepared contract by customer.",
+            })
+            .Produces(StatusCodes.Status204NoContent)
+            .Produces(StatusCodes.Status404NotFound)
+            .Produces(StatusCodes.Status409Conflict)
+            .Produces(StatusCodes.Status500InternalServerError);
     }
 }
