e634ae4168a4c5ee0e78449582b8952d06a27eb4
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2023-08-21 17:50:35 +0200

    Update ContractSignedEvent.cs

--- Chapter-3-microservice-extraction/Contracts/Src/Fitnet.Contracts.IntegrationEvents/ContractSignedEvent.cs
+++ Chapter-3-microservice-extraction/Contracts/Src/Fitnet.Contracts.IntegrationEvents/ContractSignedEvent.cs
@@ -2,7 +2,7 @@ namespace EvolutionaryArchitecture.Fitnet.Contracts.IntegrationEvents;
 
 using Common.Infrastructure.Events;
 
-public sealed record ContractSignedEvent(Guid Id, Guid ContractId, Guid ContractCustomerId, DateTimeOffset SignedAt, DateTimeOffset ExpireAt, DateTimeOffset OccurredDateTime) : IIntegrationEvent
+public record ContractSignedEvent(Guid Id, Guid ContractId, Guid ContractCustomerId, DateTimeOffset SignedAt, DateTimeOffset ExpireAt, DateTimeOffset OccurredDateTime) : IIntegrationEvent
 {
     public static ContractSignedEvent Create(Guid contractId, Guid contractCustomerId, DateTimeOffset signedAt, DateTimeOffset expireAt) =>
         new(Guid.NewGuid(), contractId, contractCustomerId, signedAt, expireAt, DateTimeOffset.UtcNow);
