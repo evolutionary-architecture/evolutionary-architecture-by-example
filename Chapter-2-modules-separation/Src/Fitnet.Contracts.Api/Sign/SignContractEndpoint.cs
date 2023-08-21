bbdcf33883e8f7cb8e69277c925e103069bab307
Maciej Jedrzejewski <mac.jedrzejewski@gmail.com>
2023-08-21 11:35:31 +0200

    feat: add swagger description for sign contract endpoint in chapter 2

--- Chapter-2-modules-separation/Src/Fitnet.Contracts.Api/Sign/SignContractEndpoint.cs
+++ Chapter-2-modules-separation/Src/Fitnet.Contracts.Api/Sign/SignContractEndpoint.cs
@@ -10,14 +10,24 @@ internal static class SignContractEndpoint
     internal static void MapSignContract(this IEndpointRouteBuilder app)
     {
         app.MapPatch(ContractsApiPaths.Sign, async (
-            Guid id, 
-            SignContractRequest request,
-            IContractsModule contractsModule, CancellationToken cancellationToken) =>
-        {
-            var command = request.ToCommand(id);
-            await contractsModule.ExecuteCommandAsync(command, cancellationToken);
-            
-            return Results.NoContent();
-        });
+                Guid id,
+                SignContractRequest request,
+                IContractsModule contractsModule, CancellationToken cancellationToken) =>
+            {
+                var command = request.ToCommand(id);
+                await contractsModule.ExecuteCommandAsync(command, cancellationToken);
+
+                return Results.NoContent();
+            })
+            .WithOpenApi(operation => new(operation)
+            {
+                Summary = "Signs prepared contract",
+                Description =
+                    "This endpoint is used to sign prepared contract by customer.",
+            })
+            .Produces(StatusCodes.Status204NoContent)
+            .Produces(StatusCodes.Status404NotFound)
+            .Produces(StatusCodes.Status409Conflict)
+            .Produces(StatusCodes.Status500InternalServerError);
     }
 }
