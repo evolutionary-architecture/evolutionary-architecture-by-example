f68aabba588f0a007ddba8244725ecd1c69ec732
Maciej Jedrzejewski <mac.jedrzejewski@gmail.com>
2023-08-21 11:39:22 +0200

    feat: add swagger description for get all passes endpoint in chapter 1

--- Chapter-1-initial-architecture/Src/Fitnet/Passes/GetAllPasses/GetAllPassesEndpoint.cs
+++ Chapter-1-initial-architecture/Src/Fitnet/Passes/GetAllPasses/GetAllPassesEndpoint.cs
@@ -8,13 +8,21 @@ internal static class GetAllPassesEndpoint
 {
     internal static void MapGetAllPasses(this IEndpointRouteBuilder app) =>
         app.MapGet(PassesApiPaths.GetAll, async (PassesPersistence persistence, CancellationToken cancellationToken) =>
-        {
-            var passes = await persistence.Passes
-                .AsNoTracking()
-                .Select(passes => PassDto.From(passes))
-                .ToListAsync(cancellationToken);
-            var response = GetAllPassesResponse.Create(passes);
-            
-            return Results.Ok(response);
-        });
+            {
+                var passes = await persistence.Passes
+                    .AsNoTracking()
+                    .Select(passes => PassDto.From(passes))
+                    .ToListAsync(cancellationToken);
+                var response = GetAllPassesResponse.Create(passes);
+
+                return Results.Ok(response);
+            })
+            .WithOpenApi(operation => new(operation)
+            {
+                Summary = "Return all passes that exist in the system",
+                Description =
+                    "This endpoint is used to retrieve all existing passes.",
+            })
+            .Produces<GetAllPassesResponse>()
+            .Produces(StatusCodes.Status500InternalServerError);
 }
