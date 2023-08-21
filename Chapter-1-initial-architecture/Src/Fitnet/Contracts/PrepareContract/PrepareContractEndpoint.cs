2223a61053bf5dec606b68a4bd7dbd0628d7939e
Maciej Jedrzejewski <mac.jedrzejewski@gmail.com>
2023-08-21 10:48:43 +0200

    feat: add swagger description for prepare contract endpoint in chapter 1

--- Chapter-1-initial-architecture/Src/Fitnet/Contracts/PrepareContract/PrepareContractEndpoint.cs
+++ Chapter-1-initial-architecture/Src/Fitnet/Contracts/PrepareContract/PrepareContractEndpoint.cs
@@ -8,18 +8,31 @@ internal static class PrepareContractEndpoint
 {
     internal static void MapPrepareContract(this IEndpointRouteBuilder app)
     {
-        app.MapPost(ContractsApiPaths.Prepare, async (PrepareContractRequest request, ContractsPersistence persistence, CancellationToken cancellationToken) =>
-        {
-            var previousContract = await GetPreviousForCustomerAsync(persistence, request.CustomerId, cancellationToken);
-            var contract = Contract.Prepare(request.CustomerId, request.CustomerAge, request.CustomerHeight, request.PreparedAt, previousContract?.Signed);
-            await persistence.Contracts.AddAsync(contract, cancellationToken);
-            await persistence.SaveChangesAsync(cancellationToken);
+        app.MapPost(ContractsApiPaths.Prepare, async (PrepareContractRequest request, ContractsPersistence persistence,
+                CancellationToken cancellationToken) =>
+            {
+                var previousContract =
+                    await GetPreviousForCustomerAsync(persistence, request.CustomerId, cancellationToken);
+                var contract = Contract.Prepare(request.CustomerId, request.CustomerAge, request.CustomerHeight,
+                    request.PreparedAt, previousContract?.Signed);
+                await persistence.Contracts.AddAsync(contract, cancellationToken);
+                await persistence.SaveChangesAsync(cancellationToken);
 
-            return Results.Created($"/{ContractsApiPaths.Prepare}/{contract.Id}", contract.Id);
-        });
+                return Results.Created($"/{ContractsApiPaths.Prepare}/{contract.Id}", contract.Id);
+            })
+            .WithOpenApi(operation => new(operation)
+            {
+                Summary = "Triggers preparation of a new contract for new or existing customer",
+                Description =
+                    "This endpoint is used to prepare a new contract for new and existing customers.",
+            })
+            .Produces<string>(StatusCodes.Status201Created)
+            .Produces(StatusCodes.Status409Conflict)
+            .Produces(StatusCodes.Status500InternalServerError);
     }
-    
-    private static async Task<Contract?> GetPreviousForCustomerAsync(ContractsPersistence persistence, Guid customerId, CancellationToken cancellationToken = default) =>
+
+    private static async Task<Contract?> GetPreviousForCustomerAsync(ContractsPersistence persistence, Guid customerId,
+        CancellationToken cancellationToken = default) =>
         await persistence.Contracts
             .OrderByDescending(contract => contract.PreparedAt)
             .SingleOrDefaultAsync(contract => contract.CustomerId == customerId, cancellationToken);
