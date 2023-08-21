47ce5fbaf78ffe56eb3c44e23339b01aef26d279
Maciej Jedrzejewski <mac.jedrzejewski@gmail.com>
2023-08-21 11:50:42 +0200

    feat: add swagger description for report endpoint in chapter 2

--- Chapter-2-modules-separation/Src/Fitnet.Reports/GenerateNewPassesRegistrationsPerMonthReport/GenerateNewPassesPerMonthReportEndpoint.cs
+++ Chapter-2-modules-separation/Src/Fitnet.Reports/GenerateNewPassesRegistrationsPerMonthReport/GenerateNewPassesPerMonthReportEndpoint.cs
@@ -10,14 +10,22 @@ internal static class GenerateNewPassesPerMonthReportEndpoint
 {
     internal static void MapGenerateNewPassesRegistrationsPerMonthReport(this IEndpointRouteBuilder app)
     {
-        app.MapGet(ReportsApiPaths.GenerateNewReport,async (
-            INewPassesRegistrationPerMonthReportDataRetriever dataRetriever, 
-            CancellationToken cancellationToken) =>
-        {
-            var reportData = await dataRetriever.GetReportDataAsync(cancellationToken);
-            var newPassesRegistrationsPerMonthResponse = NewPassesRegistrationsPerMonthResponse.Create(reportData);
-            
-            return Results.Ok(newPassesRegistrationsPerMonthResponse);
-        });
+        app.MapGet(ReportsApiPaths.GenerateNewReport, async (
+                INewPassesRegistrationPerMonthReportDataRetriever dataRetriever,
+                CancellationToken cancellationToken) =>
+            {
+                var reportData = await dataRetriever.GetReportDataAsync(cancellationToken);
+                var newPassesRegistrationsPerMonthResponse = NewPassesRegistrationsPerMonthResponse.Create(reportData);
+
+                return Results.Ok(newPassesRegistrationsPerMonthResponse);
+            })
+            .WithOpenApi(operation => new(operation)
+            {
+                Summary = "Returns report of all passes registered in a month",
+                Description =
+                    "This endpoint is used to retrieve all passes that were registered in a given month.",
+            })
+            .Produces<NewPassesRegistrationsPerMonthResponse>()
+            .Produces(StatusCodes.Status500InternalServerError);
     }
 }
