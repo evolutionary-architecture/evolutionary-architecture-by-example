b68bf68fc11364fc29409197d965dabeedd0cfa7
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2023-08-27 11:17:58 +0200

    refactor: use faker to random negative age and height

--- Chapter-1-initial-architecture/Src/Fitnet.UnitTests/Contracts/PrepareContract/RequestValidator/InvalidPrepareContractRequestTestCases.cs
+++ Chapter-1-initial-architecture/Src/Fitnet.UnitTests/Contracts/PrepareContract/RequestValidator/InvalidPrepareContractRequestTestCases.cs
@@ -4,6 +4,7 @@ using EvolutionaryArchitecture.Fitnet.Contracts.PrepareContract;
 
 internal sealed class InvalidPrepareContractRequestTestCases : IEnumerable<object[]>
 {
+    private readonly Faker _faker = new();
     private readonly DateTimeOffset _fakeNow = new Faker().Date.RecentOffset();
 
     public IEnumerator<object[]> GetEnumerator()
@@ -12,9 +13,9 @@ internal sealed class InvalidPrepareContractRequestTestCases : IEnumerable<objec
 
         yield return new object[] { new PrepareContractRequest(Guid.Empty, validContractParameters.MinAge, validContractParameters.MaxHeight, _fakeNow), nameof(PrepareContractRequest.CustomerId) };
         yield return new object[] { new PrepareContractRequest(Guid.NewGuid(), default, validContractParameters.MaxHeight, _fakeNow), nameof(PrepareContractRequest.CustomerAge) };
-        yield return new object[] { new PrepareContractRequest(Guid.NewGuid(), -1, validContractParameters.MaxHeight, _fakeNow), nameof(PrepareContractRequest.CustomerAge) };
+        yield return new object[] { new PrepareContractRequest(Guid.NewGuid(), _faker.Random.Number(-100,-1), validContractParameters.MaxHeight, _fakeNow), nameof(PrepareContractRequest.CustomerAge) };
         yield return new object[] { new PrepareContractRequest(Guid.NewGuid(), validContractParameters.MinAge, default, _fakeNow), nameof(PrepareContractRequest.CustomerHeight) };
-        yield return new object[] { new PrepareContractRequest(Guid.NewGuid(), validContractParameters.MinAge, -1, _fakeNow), nameof(PrepareContractRequest.CustomerHeight) };
+        yield return new object[] { new PrepareContractRequest(Guid.NewGuid(), validContractParameters.MinAge, _faker.Random.Number(-100,-1), _fakeNow), nameof(PrepareContractRequest.CustomerHeight) };
         yield return new object[] { new PrepareContractRequest(Guid.NewGuid(), validContractParameters.MinAge, validContractParameters.MaxHeight, default),  nameof(PrepareContractRequest.PreparedAt) };
         yield return new object[] { new PrepareContractRequest(Guid.NewGuid(), validContractParameters.MinAge, validContractParameters.MaxHeight, default),  nameof(PrepareContractRequest.PreparedAt) };
     }
