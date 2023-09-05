b60b33e04ef9600b26e6b8fa9c3965a40138a4fa
Maciej Jedrzejewski <mac.jedrzejewski@gmail.com>
2023-09-05 09:02:59 +0200

    fix: issues related to static code analysis in common module

--- Chapter-2-modules-separation/Src/Common/Fitnet.Common.IntegrationTests/TestEngine/FitnetWebApplicationFactory.cs
+++ Chapter-2-modules-separation/Src/Common/Fitnet.Common.IntegrationTests/TestEngine/FitnetWebApplicationFactory.cs
@@ -2,9 +2,8 @@ namespace EvolutionaryArchitecture.Fitnet.Common.IntegrationTests.TestEngine;
 
 public class FitnetWebApplicationFactory<T> : WebApplicationFactory<T> where T : class
 {
-    protected override void ConfigureWebHost(IWebHostBuilder builder)
-    {
-        builder.ConfigureAppConfiguration((_, configBuilder) =>
+    protected override void ConfigureWebHost(IWebHostBuilder builder) => builder.ConfigureAppConfiguration(
+        (_, configBuilder) =>
         {
             var settingsPath = Path.Combine(
                 Directory.GetCurrentDirectory(),
@@ -12,5 +11,4 @@ public class FitnetWebApplicationFactory<T> : WebApplicationFactory<T> where T :
 
             configBuilder.AddJsonFile(settingsPath);
         });
-    }
 }
