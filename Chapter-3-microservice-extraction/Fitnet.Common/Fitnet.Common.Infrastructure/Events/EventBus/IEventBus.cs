ee75356ba2c246ba706c37f07df4fc735c99c8b0
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2023-11-09 20:57:44 +0100

    feat: remove event bus abstraction

--- Chapter-3-microservice-extraction/Fitnet.Common/Fitnet.Common.Infrastructure/Events/EventBus/IEventBus.cs
+++
@@ -1,7 +0,0 @@
-namespace EvolutionaryArchitecture.Fitnet.Common.Infrastructure.Events.EventBus;
-
-public interface IEventBus
-{
-    Task PublishAsync<TEvent>(TEvent @event, CancellationToken cancellationToken = default)
-        where TEvent : IIntegrationEvent;
-}
