b7b26d9c7838182941bf3a8478df08c32deceac1
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2023-11-09 21:11:02 +0100

    refactor: remove EventBus and use MassTransit for event publishing
    Deprecated the EventBus as we have chosen to directly use MassTransit's `IPublishEndpoint` instead for event publishing. All dependency on the previously implemented EventBus, along with the EventBusOptions class, has been removed across the codebase. The handlers and consumers have been refactored accordingly.

--- Chapter-3-microservice-extraction/Fitnet.Contracts/Src/Fitnet.Contracts.Application/Sign/SignContractCommandHandler.cs
+++ Chapter-3-microservice-extraction/Fitnet.Contracts/Src/Fitnet.Contracts.Application/Sign/SignContractCommandHandler.cs
@@ -2,34 +2,38 @@ namespace EvolutionaryArchitecture.Fitnet.Contracts.Application.Sign;
 
 using Common.Api.ErrorHandling;
 using Common.Core.SystemClock;
-using Common.Infrastructure.Events.EventBus;
 using Core;
 using IntegrationEvents;
+using MassTransit;
 
 [UsedImplicitly]
 internal sealed class SignContractCommandHandler : IRequestHandler<SignContractCommand>
 {
     private readonly IContractsRepository _contractsRepository;
     private readonly ISystemClock _systemClock;
-    private readonly IEventBus _eventBus;
+    private readonly IPublishEndpoint _publishEndpoint;
 
-    public SignContractCommandHandler(IContractsRepository contractsRepository, ISystemClock systemClock, IEventBus eventBus)
+    public SignContractCommandHandler(
+        IContractsRepository contractsRepository,
+        ISystemClock systemClock,
+        IPublishEndpoint publishEndpoint)
     {
         _contractsRepository = contractsRepository;
         _systemClock = systemClock;
-        _eventBus = eventBus;
+        _publishEndpoint = publishEndpoint;
     }
 
     public async Task Handle(SignContractCommand command, CancellationToken cancellationToken)
     {
         var contract = await _contractsRepository.GetByIdAsync(command.Id, cancellationToken) ??
                        throw new ResourceNotFoundException(command.Id);
+
         contract.Sign(command.SignedAt, _systemClock.Now);
         await _contractsRepository.CommitAsync(cancellationToken);
         var @event = ContractSignedEvent.Create(contract.Id,
                                                         contract.CustomerId,
                                                         contract.SignedAt!.Value,
                                                         contract.ExpiringAt!.Value);
-        await _eventBus.PublishAsync(@event, cancellationToken);
+        await _publishEndpoint.Publish(@event, cancellationToken);
     }
 }
