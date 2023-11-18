ba0bc7ddf3d848d5df861a10bf31e9e6dce5a427
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2023-11-18 10:46:56 +0100

    fix: method name formatting in ContractSignedEvent

--- Chapter-3-microservice-extraction/Fitnet.Contracts/Src/Fitnet.Contracts.IntegrationEvents/ContractSignedEvent.cs
+++ Chapter-3-microservice-extraction/Fitnet.Contracts/Src/Fitnet.Contracts.IntegrationEvents/ContractSignedEvent.cs
@@ -2,6 +2,6 @@ namespace EvolutionaryArchitecture.Fitnet.Contracts.IntegrationEvents;
 
 public sealed record ContractSignedEvent(Guid Id, Guid ContractId, Guid ContractCustomerId, DateTimeOffset SignedAt, DateTimeOffset ExpireAt, DateTimeOffset OccurredDateTime)
 {
-    public static ContractSignedEvent ````Create(Guid contractId, Guid contractCustomerId, DateTimeOffset signedAt, DateTimeOffset expireAt) =>
+    public static ContractSignedEvent Create(Guid contractId, Guid contractCustomerId, DateTimeOffset signedAt, DateTimeOffset expireAt) =>
         new(Guid.NewGuid(), contractId, contractCustomerId, signedAt, expireAt, DateTimeOffset.UtcNow);
 }
