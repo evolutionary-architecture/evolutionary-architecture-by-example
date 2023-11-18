4214005e3eefdbe69a507a8f24b7032185dc7d6c
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2023-11-18 10:24:55 +0100

    refactor: test classes and improve syntax usage
    The commit includes syntax improvements in several test classes for better readability and easier maintenance. It optimizes SignContractTestData's data structure initialization by switching to array notation. In addition, it refactors InMemoryEventBusTests and ExceptionMiddlewareTests to streamline the way they use constructors and methods to enhance the code's understandability. It also ensures the presence of a newline at the end of some files.

--- Chapter-1-initial-architecture/Src/Fitnet.UnitTests/Contracts/SignContract/SignContractTestData.cs
+++ Chapter-1-initial-architecture/Src/Fitnet.UnitTests/Contracts/SignContract/SignContractTestData.cs
@@ -2,30 +2,26 @@ namespace EvolutionaryArchitecture.Fitnet.UnitTests.Contracts.SignContract;
 
 internal sealed class SignContractTestData : IEnumerable<object[]>
 {
-    private readonly List<object[]> _data = new()
-    {
-        new object[]
-        {
+    private readonly List<object[]> _data = [
+        [
             new DateTimeOffset(2021, 1, 1, 1, 1, 1, TimeSpan.Zero),
             new DateTimeOffset(2021, 1, 1, 1, 1, 1, TimeSpan.Zero),
             new DateTimeOffset(2021, 1, 1, 1, 1, 1, TimeSpan.Zero),
             new DateTimeOffset(2022, 1, 1, 1, 1, 1, TimeSpan.Zero)
-        },
-        new object[]
-        {
+        ],
+        [
             new DateTimeOffset(2022, 6, 1, 12, 0, 0, TimeSpan.Zero),
             new DateTimeOffset(2022, 6, 1, 12, 0, 0, TimeSpan.Zero),
             new DateTimeOffset(2022, 6, 1, 12, 0, 0, TimeSpan.Zero),
             new DateTimeOffset(2023, 6, 1, 12, 0, 0, TimeSpan.Zero)
-        },
-        new object[]
-        {
+        ],
+        [
             new DateTimeOffset(2023, 2, 15, 8, 30, 0, TimeSpan.Zero),
             new DateTimeOffset(2023, 2, 15, 8, 30, 0, TimeSpan.Zero),
             new DateTimeOffset(2023, 2, 15, 8, 30, 0, TimeSpan.Zero),
             new DateTimeOffset(2024, 2, 15, 8, 30, 0, TimeSpan.Zero)
-        }
-    };
+        ]
+    ];
 
     public IEnumerator<object[]> GetEnumerator() => _data.GetEnumerator();
 
