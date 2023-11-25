20f98d2d01986d9fec0e547abf1cad157e50cafd
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2023-11-25 08:36:37 +0100

    feat: upgrade to .net 8

--- Chapter-3-microservice-extraction/Fitnet.Contracts/Src/Fitnet.Contracts.Application/Sign/SignContractCommandHandler.cs
+++ Chapter-3-microservice-extraction/Fitnet.Contracts/Src/Fitnet.Contracts.Application/Sign/SignContractCommandHandler.cs
@@ -7,32 +7,21 @@ using IntegrationEvents;
 using MassTransit;
 
 [UsedImplicitly]
-internal sealed class SignContractCommandHandler : IRequestHandler<SignContractCommand>
+internal sealed class SignContractCommandHandler(
+    IContractsRepository contractsRepository,
+    ISystemClock systemClock,
+    IPublishEndpoint publishEndpoint) : IRequestHandler<SignContractCommand>
 {
-    private readonly IContractsRepository _contractsRepository;
-    private readonly ISystemClock _systemClock;
-    private readonly IPublishEndpoint _publishEndpoint;
-
-    public SignContractCommandHandler(
-        IContractsRepository contractsRepository,
-        ISystemClock systemClock,
-        IPublishEndpoint publishEndpoint)
-    {
-        _contractsRepository = contractsRepository;
-        _systemClock = systemClock;
-        _publishEndpoint = publishEndpoint;
-    }
-
     public async Task Handle(SignContractCommand command, CancellationToken cancellationToken)
     {
-        var contract = await _contractsRepository.GetByIdAsync(command.Id, cancellationToken) ??
+        var contract = await contractsRepository.GetByIdAsync(command.Id, cancellationToken) ??
                        throw new ResourceNotFoundException(command.Id);
-        contract.Sign(command.SignedAt, _systemClock.Now);
-        await _contractsRepository.CommitAsync(cancellationToken);
+        contract.Sign(command.SignedAt, systemClock.Now);
+        await contractsRepository.CommitAsync(cancellationToken);
         var @event = ContractSignedEvent.Create(contract.Id,
                                                         contract.CustomerId,
                                                         contract.SignedAt!.Value,
                                                         contract.ExpiringAt!.Value);
-        await _publishEndpoint.Publish(@event, cancellationToken);
+        await publishEndpoint.Publish(@event, cancellationToken);
     }
 }
