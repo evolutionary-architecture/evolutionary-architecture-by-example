00509c86cc1d6e9d9309611a39276ede8a9a91c7
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2023-11-27 19:44:41 +0100

    refactor: SignContractTestData and update global usings
    Simplified the SignContractTestData class by changing it from a list of object arrays to inherit from TheoryData. Removed unnecessary implementation of IEnumerator. In the GlobalUsings.cs file, removed an unused global using declaration.

--- Chapter-3-microservice-extraction/Fitnet.Contracts/Src/Fitnet.Contracts.Core.UnitTests/SignContract/SignContractTestData.cs
+++ Chapter-3-microservice-extraction/Fitnet.Contracts/Src/Fitnet.Contracts.Core.UnitTests/SignContract/SignContractTestData.cs
@@ -1,30 +1,26 @@
 namespace EvolutionaryArchitecture.Fitnet.Contracts.Core.UnitTests.SignContract;
 
-internal sealed class SignContractTestData : IEnumerable<object[]>
+internal sealed class SignContractTestData : TheoryData<DateTimeOffset, DateTimeOffset, DateTimeOffset, DateTimeOffset>
 {
-    private readonly List<object[]> _data =
-    [
-        [
+    public SignContractTestData()
+    {
+        AddRow(
             new DateTimeOffset(2021, 1, 1, 1, 1, 1, TimeSpan.Zero),
             new DateTimeOffset(2021, 1, 1, 1, 1, 1, TimeSpan.Zero),
             new DateTimeOffset(2021, 1, 1, 1, 1, 1, TimeSpan.Zero),
             new DateTimeOffset(2022, 1, 1, 1, 1, 1, TimeSpan.Zero)
-        ],
-        [
+        );
+        AddRow(
             new DateTimeOffset(2022, 6, 1, 12, 0, 0, TimeSpan.Zero),
             new DateTimeOffset(2022, 6, 1, 12, 0, 0, TimeSpan.Zero),
             new DateTimeOffset(2022, 6, 1, 12, 0, 0, TimeSpan.Zero),
             new DateTimeOffset(2023, 6, 1, 12, 0, 0, TimeSpan.Zero)
-        ],
-        [
+        );
+        AddRow(
             new DateTimeOffset(2023, 2, 15, 8, 30, 0, TimeSpan.Zero),
             new DateTimeOffset(2023, 2, 15, 8, 30, 0, TimeSpan.Zero),
             new DateTimeOffset(2023, 2, 15, 8, 30, 0, TimeSpan.Zero),
             new DateTimeOffset(2024, 2, 15, 8, 30, 0, TimeSpan.Zero)
-        ]
-    ];
-
-    public IEnumerator<object[]> GetEnumerator() => _data.GetEnumerator();
-
-    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        );
+    }
 }
