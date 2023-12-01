b8eee59bf0c798405f4f74ce12b3f4d4d9eb7ce6
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2023-12-01 16:48:00 +0100

    refactor: use theory data

--- Chapter-2-modules-separation/Src/Contracts/Tests/Fitnet.Contracts.Core.UnitTests/SignContract/SignContractTestData.cs
+++ Chapter-2-modules-separation/Src/Contracts/Tests/Fitnet.Contracts.Core.UnitTests/SignContract/SignContractTestData.cs
@@ -1,29 +1,22 @@
 namespace EvolutionaryArchitecture.Fitnet.Contracts.Core.UnitTests.SignContract;
 
-internal static class SignContractTestData
+internal sealed class SignContractTestData : TheoryData<DateTimeOffset, DateTimeOffset, DateTimeOffset, DateTimeOffset>
 {
-    public static IEnumerable<object[]> GetDates()
+    public SignContractTestData()
     {
-        yield return new object[]
-        {
+        Add(new DateTimeOffset(2021, 1, 1, 1, 1, 1, TimeSpan.Zero),
             new DateTimeOffset(2021, 1, 1, 1, 1, 1, TimeSpan.Zero),
             new DateTimeOffset(2021, 1, 1, 1, 1, 1, TimeSpan.Zero),
-            new DateTimeOffset(2021, 1, 1, 1, 1, 1, TimeSpan.Zero),
-            new DateTimeOffset(2022, 1, 1, 1, 1, 1, TimeSpan.Zero)
-        };
-        yield return new object[]
-        {
+            new DateTimeOffset(2022, 1, 1, 1, 1, 1, TimeSpan.Zero));
+
+        Add(new DateTimeOffset(2022, 6, 1, 12, 0, 0, TimeSpan.Zero),
             new DateTimeOffset(2022, 6, 1, 12, 0, 0, TimeSpan.Zero),
             new DateTimeOffset(2022, 6, 1, 12, 0, 0, TimeSpan.Zero),
-            new DateTimeOffset(2022, 6, 1, 12, 0, 0, TimeSpan.Zero),
-            new DateTimeOffset(2023, 6, 1, 12, 0, 0, TimeSpan.Zero)
-        };
-        yield return new object[]
-        {
+            new DateTimeOffset(2023, 6, 1, 12, 0, 0, TimeSpan.Zero));
+
+        Add(new DateTimeOffset(2023, 2, 15, 8, 30, 0, TimeSpan.Zero),
             new DateTimeOffset(2023, 2, 15, 8, 30, 0, TimeSpan.Zero),
             new DateTimeOffset(2023, 2, 15, 8, 30, 0, TimeSpan.Zero),
-            new DateTimeOffset(2023, 2, 15, 8, 30, 0, TimeSpan.Zero),
-            new DateTimeOffset(2024, 2, 15, 8, 30, 0, TimeSpan.Zero)
-        };
+            new DateTimeOffset(2024, 2, 15, 8, 30, 0, TimeSpan.Zero));
     }
 }
