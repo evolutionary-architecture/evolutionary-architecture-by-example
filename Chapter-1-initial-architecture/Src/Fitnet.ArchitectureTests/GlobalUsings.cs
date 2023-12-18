8648e5e2c56905d21fce0c5bbc103a56875b45ce
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2023-12-18 21:02:23 +0100

    feat: add architecture contracts

--- Chapter-1-initial-architecture/Src/Fitnet.ArchitectureTests/GlobalUsings.cs
+++ Chapter-1-initial-architecture/Src/Fitnet.ArchitectureTests/GlobalUsings.cs
@@ -1,5 +1,2 @@
-global using System.Reflection;
-global using EvolutionaryArchitecture.Fitnet.ArchitectureTests.Common.Predicates;
 global using FluentAssertions;
-global using NetArchTest.Rules;
 global using Xunit;
