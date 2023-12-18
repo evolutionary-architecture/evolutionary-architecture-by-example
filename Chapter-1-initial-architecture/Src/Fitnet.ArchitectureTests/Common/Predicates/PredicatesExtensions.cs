6d87fe8cc30a6351b5fa0b9797b8423018591439
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2023-12-18 21:02:23 +0100

    feat: add architecture contracts

--- Chapter-1-initial-architecture/Src/Fitnet.ArchitectureTests/Common/Predicates/PredicatesExtensions.cs
+++ Chapter-1-initial-architecture/Src/Fitnet.ArchitectureTests/Common/Predicates/PredicatesExtensions.cs
@@ -1,11 +1,13 @@
 ﻿namespace EvolutionaryArchitecture.Fitnet.ArchitectureTests.Common.Predicates;
 
+using NetArchTest.Rules;
+
 internal static class PredicatesExtensions
 {
-    internal static string[] GetModuleTypes(this PredicateList predicates) =>
+    internal static string[] GetNamespaces(this PredicateList predicates) =>
         predicates
             .GetTypes()
-            .Select(type => type.FullName)
+            .Select(type => type.Namespace)
             .Distinct()
             .ToArray()!;
 }
---
+++ Chapter-1-initial-architecture/Src/Fitnet.ArchitectureTests/Contracts/ContractsModuleArchitectureTests.cs
@@ -0,0 +1,37 @@
+﻿namespace EvolutionaryArchitecture.Fitnet.ArchitectureTests.Contracts;
+
+using System.Reflection;
+using Common.Predicates;
+using NetArchTest.Rules;
+
+public class ContractsModuleArchitectureTests
+{
+    private const string Contracts = "EvolutionaryArchitecture.Fitnet.Contracts";
+    private const string Event = "Event";
+    private readonly Assembly _solution = typeof(Program).Assembly;
+
+    [Fact]
+    internal void Modules_Should_Only_Communicate_With_Contracts_Module_Via_Events()
+    {
+        // Arrange
+        var contractsModule = Types.InAssembly(_solution)
+            .That()
+            .ResideInNamespace(Contracts)
+            .And()
+            .DoNotHaveNameEndingWith(Event);
+        var contractModuleNamespaces = contractsModule.GetNamespaces();
+
+        var othersModules = Types.InAssembly(_solution)
+            .That()
+            .DoNotResideInNamespace(Contracts);
+
+        // Act
+        var rules = othersModules
+                                            .Should()
+                                            .NotHaveDependencyOnAny(contractModuleNamespaces);
+        var validationResult = rules!.GetResult();
+
+        // Assert
+        validationResult.IsSuccessful.Should()!.BeTrue();
+    }
+}
