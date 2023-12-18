f1470030dc3a1110a7a8283eeac6c964b18ed422
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2023-12-18 21:02:23 +0100

    feat: add architecture contracts

---
+++ Chapter-1-initial-architecture/Src/Fitnet.ArchitectureTests/Common/Predicates/PredicatesExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace EvolutionaryArchitecture.Fitnet.ArchitectureTests.Common.Predicates;
+
+using NetArchTest.Rules;
+
+internal static class PredicatesExtensions
+{
+    internal static string[] GetNamespaces(this PredicateList predicates) =>
+        predicates
+            .GetTypes()
+            .Select(type => type.Namespace)
+            .Distinct()
+            .ToArray()!;
+}
