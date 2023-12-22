6df83e42f08f58b87931cbb2ce3a474796803359
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2023-12-22 19:50:05 +0100

    feat: implement dependecy checking for each module

--- Chapter-1-initial-architecture/Src/Fitnet.ArchitectureTests/Common/Predicates/PredicatesExtensions.cs
+++ Chapter-1-initial-architecture/Src/Fitnet.ArchitectureTests/Common/Predicates/PredicatesExtensions.cs
@@ -1,7 +1,5 @@
 ﻿namespace EvolutionaryArchitecture.Fitnet.ArchitectureTests.Common.Predicates;
 
-using NetArchTest.Rules;
-
 internal static class PredicatesExtensions
 {
     internal static string[] GetModuleTypes(this PredicateList predicates) =>
