c05422d8c0f0062bd35eb194dfc1990bc448d984
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2023-12-22 19:50:05 +0100

    feat: implement dependecy checking for each module

--- Chapter-1-initial-architecture/Src/Fitnet.ArchitectureTests/GlobalUsings.cs
+++ Chapter-1-initial-architecture/Src/Fitnet.ArchitectureTests/GlobalUsings.cs
@@ -1,2 +1,5 @@
+global using System.Reflection;
+global using EvolutionaryArchitecture.Fitnet.ArchitectureTests.Common.Predicates;
 global using FluentAssertions;
+global using NetArchTest.Rules;
 global using Xunit;
