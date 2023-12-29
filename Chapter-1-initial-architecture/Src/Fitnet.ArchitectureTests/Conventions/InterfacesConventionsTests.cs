6d408d58d49e86f01fa03058f66fbe7f9b73f893
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2023-12-29 11:43:17 +0100

    feat: add interfaces should start from I convention test

---
+++ Chapter-1-initial-architecture/Src/Fitnet.ArchitectureTests/Conventions/InterfacesConventionsTests.cs
@@ -0,0 +1,22 @@
+﻿namespace EvolutionaryArchitecture.Fitnet.ArchitectureTests.Conventions;
+
+using Common;
+
+public sealed class InterfacesConventionsTests
+{
+    [Fact]
+    internal void ShouldStartWithI()
+    {
+        // Arrange
+        var rules = Solution.Types.That()
+            .AreInterfaces()
+            .Should()
+            .HaveNameStartingWith("I");
+
+        // Act
+        var result = rules.GetResult();
+
+        // Assert
+        result.IsSuccessful.Should().BeTrue();
+    }
+}
