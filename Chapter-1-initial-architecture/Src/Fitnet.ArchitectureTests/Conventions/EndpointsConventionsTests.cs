ea1217b6f8802eb206334ccbac9090643d36a26a
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2023-12-29 11:42:55 +0100

    feat: add endpoints should be static convention test

---
+++ Chapter-1-initial-architecture/Src/Fitnet.ArchitectureTests/Conventions/EndpointsConventionsTests.cs
@@ -0,0 +1,26 @@
+﻿namespace EvolutionaryArchitecture.Fitnet.ArchitectureTests.Conventions;
+
+using Common;
+
+public sealed class EndpointsConventionsTests
+{
+    private const string Endpoint = "Endpoint";
+
+    [Fact]
+    internal void ShouldBeStatic()
+    {
+        // Arrange
+        var rules = Solution.Types.That()
+            .AreClasses()
+            .And()
+            .HaveNameEndingWith(Endpoint)
+            .Should()
+            .BeStatic();
+
+        // Act
+        var result = rules.GetResult();
+
+        // Assert
+        result.IsSuccessful.Should().BeTrue();
+    }
+}
