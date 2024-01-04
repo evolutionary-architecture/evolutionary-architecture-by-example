3466dcde3e527fb65c8c28ac297ea0e77a124b28
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2024-01-04 08:12:54 +0100

    feat: add bussiness rules arch tests

---
+++ Chapter-1-initial-architecture/Src/Fitnet.ArchitectureTests/BussinessRulesArchitectureTests.cs
@@ -0,0 +1,26 @@
+﻿namespace EvolutionaryArchitecture.Fitnet.ArchitectureTests;
+
+using Common;
+using Fitnet.Common.BusinessRulesEngine;
+
+public class BusinessRulesArchitectureTests
+{
+    [Theory]
+    [InlineData("Endpoint")]
+    [InlineData("Consumer")]
+    internal void BusinessRules_Should_Not_Have_Dependency_On_Module(string forbiddenTypeEnding)
+    {
+        // Arrange
+        var businessRules = Solution.Types.That().ImplementInterface(typeof(IBusinessRule)).GetModuleTypes();
+        var forbiddenTypes = Solution
+            .Types
+            .That()
+            .HaveNameEndingWith(forbiddenTypeEnding);
+
+        // Act
+        var validationResult = forbiddenTypes!.Should().NotHaveDependencyOnAny(businessRules).GetResult();
+
+        // Assert
+        validationResult.FailingTypes.Should().BeNull();
+    }
+}
