21637d39866a70ceb72c64b30a2e9c47aa0c7d0d
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2024-02-01 07:43:05 +0100

    feat: add ContractMustNotBeAlreadySigned business rule

---
+++ Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Core.UnitTests/SignContract/BusinessRules/ContractMustNotBeAlreadySignedTests.cs
@@ -0,0 +1,37 @@
+﻿namespace EvolutionaryArchitecture.Fitnet.Contracts.Core.UnitTests.SignContract.BusinessRules;
+
+using Core.SignContract.BusinessRules;
+using EvolutionaryArchitecture.Fitnet.Common.Core.BusinessRules;
+
+public sealed class ContractMustNotBeAlreadySignedTests
+{
+    [Fact]
+    internal void Given_unsigned_contract_and_attempt_to_sign_When_contract_is_already_signed_Then_validation_should_throw()
+    {
+        // Arrange
+        var signed = false;
+
+        // Act
+        var act = () => BusinessRuleValidator.Validate(new ContractMustNotBeAlreadySigned(signed));
+
+        // Assert
+        act.Should()
+            .Throw<BusinessRuleValidationException>()
+            .WithMessage("Contract is already signed");
+    }
+
+    [Fact]
+    internal void Given_unsigned_contract_and_attempt_to_sign_Then_pass_validation()
+    {
+        // Arrange
+        var signed = true;
+
+        // Act
+        var act = () =>
+            BusinessRuleValidator.Validate(
+                new ContractMustNotBeAlreadySigned(signed));
+
+        // Assert
+        act.Should().NotThrow<BusinessRuleValidationException>();
+    }
+}
--- Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Core/ContractAlreadySignedException.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace EvolutionaryArchitecture.Fitnet.Contracts.Core;
-
-internal sealed class ContractAlreadySignedException() : InvalidOperationException("Contract is already signed");
