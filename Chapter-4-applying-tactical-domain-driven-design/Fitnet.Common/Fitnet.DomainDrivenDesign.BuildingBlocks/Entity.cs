c28ea5a0c338b0d7c64842fdf64029009973c3a0
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2024-02-02 22:54:23 +0100

    feat: add entity building block

---
+++ Chapter-4-applying-tactical-domain-driven-design/Fitnet.Common/Fitnet.DomainDrivenDesign.BuildingBlocks/Entity.cs
@@ -0,0 +1,13 @@
+﻿namespace EvolutionaryArchitecture.Fitnet.DomainDrivenDesign.BuildingBlocks;
+
+public abstract class Entity
+{
+    private List<IDomainEvent>? _events;
+    public IReadOnlyCollection<IDomainEvent> Events => _events?.AsReadOnly() ?? new List<IDomainEvent>().AsReadOnly();
+
+    protected void RecordEvent(IDomainEvent domainEvent)
+    {
+        _events ??= [];
+        _events.Add(domainEvent);
+    }
+}
