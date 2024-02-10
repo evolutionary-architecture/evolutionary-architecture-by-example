759e04fd188aa7b41b68ea303649a926be8e71c9
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2024-02-10 08:49:49 +0100

    feat: add termination binding contract

---
+++ Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Core.UnitTests/TerminateBindingContract/BusinessRules/TerminationCannotBeBefore3MonthsFromStartBindingContract.cs
@@ -0,0 +1,11 @@
+﻿namespace EvolutionaryArchitecture.Fitnet.Contracts.Core.UnitTests.TerminateBindingContract.BusinessRules;
+
+using Common.Core.BusinessRules;
+
+internal sealed class TerminationCannotBeBefore3MonthsFromStartBindingContract(DateTimeOffset signDateTimeOffset, DateTimeOffset now)
+    : IBusinessRule
+{
+    public bool IsMet() => now - signDateTimeOffset > TimeSpan.FromDays(90);
+
+    public string Error => "";
+}
