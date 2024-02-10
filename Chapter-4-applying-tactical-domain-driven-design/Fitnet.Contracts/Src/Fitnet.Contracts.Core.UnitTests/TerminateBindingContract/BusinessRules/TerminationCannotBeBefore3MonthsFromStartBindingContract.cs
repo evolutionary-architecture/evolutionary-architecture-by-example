220dc59f6787803e685d3c62c8e4fe5d92d26a20
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2024-02-10 11:21:29 +0100

    fix: termination binding contract violated message

--- Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Core.UnitTests/TerminateBindingContract/BusinessRules/TerminationCannotBeBefore3MonthsFromStartBindingContract.cs
+++ Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Core.UnitTests/TerminateBindingContract/BusinessRules/TerminationCannotBeBefore3MonthsFromStartBindingContract.cs
@@ -5,7 +5,18 @@ using Common.Core.BusinessRules;
 internal sealed class TerminationCannotBeBefore3MonthsFromStartBindingContract(DateTimeOffset signDateTimeOffset, DateTimeOffset now)
     : IBusinessRule
 {
-    public bool IsMet() => now - signDateTimeOffset > TimeSpan.FromDays(90);
+    private readonly TimeSpan _3Months = TimeSpan.FromDays(90);
 
-    public string Error => "";
+    public bool IsMet() => now - signDateTimeOffset > _3Months;
+
+    public string Error
+    {
+        get
+        {
+            var daysLeft = (_3Months - (now - signDateTimeOffset)).Days;
+            var error = $"Termination is not possible until three months have elapsed. {daysLeft} days remaining.";
+
+            return error;
+        }
+    }
 }
