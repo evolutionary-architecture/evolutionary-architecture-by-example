416b3807c9b87acbcfd81cc60c5951e8fbd7e7bd
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2024-02-11 12:07:42 +0100

    feat: add binding contract 3 months not passed postive test case

---
+++ Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Core.UnitTests/TerminateBindingContract/BusinessRules/TerminationCannotBeBefore3MonthsFromStartBindingContractTests.cs
@@ -0,0 +1,39 @@
+﻿namespace EvolutionaryArchitecture.Fitnet.Contracts.Core.UnitTests.TerminateBindingContract.BusinessRules;
+
+using BindingContract3MonthsNotPassed.TestData;
+using Common.Core.BusinessRules;
+
+public sealed class TerminationCannotBeBefore3MonthsFromStartBindingContractTests
+{
+    [Theory]
+    [ClassData(typeof(BindingContract3MonthsNotPassedTestData))]
+    internal void Given_terminate_When_3_months_not_passed_Then_it_is_not_possible_to_terminate(DateTimeOffset signDate,
+        DateTimeOffset nowDate)
+    {
+        // Arrange
+
+        // Act
+        var act = () =>
+            BusinessRuleValidator.Validate(
+                new TerminationCannotBeBefore3MonthsFromStartBindingContract(signDate, nowDate));
+
+        // Assert
+        act.Should().Throw<BusinessRuleValidationException>();
+    }
+
+    [Theory]
+    [ClassData(typeof(BindingContract3MonthsPassedTestData))]
+    internal void Given_terminate_When_3_months_passed_Then_it_is_possible_to_terminate(DateTimeOffset signDate,
+        DateTimeOffset nowDate)
+    {
+        // Arrange
+
+        // Act
+        var act = () =>
+            BusinessRuleValidator.Validate(
+                new TerminationCannotBeBefore3MonthsFromStartBindingContract(signDate, nowDate));
+
+        // Assert
+        act.Should().NotThrow<BusinessRuleValidationException>();
+    }
+}
