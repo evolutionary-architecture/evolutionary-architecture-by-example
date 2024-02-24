11ac2a4e79783d9c9af81d5c70ee6be99a4f0c3c
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2024-02-24 13:45:19 +0100

    tests: add tests for terminate binding contract

---
+++ Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.IntegrationTests/SignContract/SignContractTestExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace EvolutionaryArchitecture.Fitnet.Contracts.IntegrationTests.SignContract;
+
+using Api.Sign;
+
+internal static class SignContractTestExtensions
+{
+    public static async Task SignContractAsync(this HttpClient httpClient, Guid contractId)
+    {
+        var requestParameters = SignContractRequestParameters.GetValid(contractId);
+        var signContractRequest = new SignContractRequest(requestParameters.SignedAt);
+        var response = await httpClient.PatchAsJsonAsync(requestParameters.Url, signContractRequest);
+        response.EnsureSuccessStatusCode();
+    }
+}
