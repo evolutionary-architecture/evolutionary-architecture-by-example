84bea59f258b728ea368b28541e02f8eae29eba9
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2024-03-02 10:30:36 +0100

    feat: add system time provider configuration in test possibility

---
+++ Chapter-4-applying-tactical-domain-driven-design/Fitnet.Common/Fitnet.Common.IntegrationTestsToolbox/TestEngine/Time/TimeExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace EvolutionaryArchitecture.Fitnet.Common.IntegrationTestsToolbox.TestEngine.SystemClock;
+
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+
+public static class TimeExtensions
+{
+    public static WebApplicationFactory<T> WithTime<T>(
+        this WebApplicationFactory<T> webApplicationFactory, FakeSystemTimeProvider fakeSystemTimeProvider)
+        where T : class => webApplicationFactory
+        .WithWebHostBuilder(builder => builder.ConfigureTestServices(services => services.AddSingleton<TimeProvider>(fakeSystemTimeProvider)));
+}
