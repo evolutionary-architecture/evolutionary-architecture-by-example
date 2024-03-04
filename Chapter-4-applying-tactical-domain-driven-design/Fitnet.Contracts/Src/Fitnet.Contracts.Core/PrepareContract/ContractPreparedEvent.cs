7f81e6f0e4f809025b0b0f0d96c8e547adc43747
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2024-03-04 20:40:20 +0100

    feat: create domain events

---
+++ Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Core/PrepareContract/ContractPreparedEvent.cs
@@ -0,0 +1,18 @@
+﻿namespace EvolutionaryArchitecture.Fitnet.Contracts.Core.PrepareContract;
+
+using DomainDrivenDesign.BuildingBlocks;
+
+public sealed record ContractPreparedEvent(
+    Guid Id,
+    Guid CustomerId,
+    DateTimeOffset PreparedAt,
+    DateTime OccuredAt) : IDomainEvent
+{
+    internal static ContractPreparedEvent Create(Guid customerId,
+        DateTimeOffset preparedAt)
+        => new(
+            Guid.NewGuid(),
+            customerId,
+            preparedAt,
+            DateTime.Now);
+}
