aafd8e2add95644841996f1ee9a5c5bec1fc9ea7
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2024-03-04 20:40:20 +0100

    feat: create domain events

---
+++ Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Core/SignContract/ContractStartedBindingEvent.cs
@@ -0,0 +1,14 @@
+﻿namespace EvolutionaryArchitecture.Fitnet.Contracts.Core.SignContract;
+
+using DomainDrivenDesign.BuildingBlocks;
+
+public sealed record ContractStartedBindingEvent(
+    Guid Id,
+    DateTimeOffset BindingFrom,
+    DateTimeOffset? ExpiringAt,
+    DateTime OccuredAt) : IDomainEvent
+{
+    internal static ContractStartedBindingEvent Create(DateTimeOffset bindingFrom,
+        DateTimeOffset? expiringAt)
+        => new(Guid.NewGuid(), bindingFrom, expiringAt, DateTime.Now);
+}
---
+++ Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Core/TerminateContract/BindingContractTerminatedEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace EvolutionaryArchitecture.Fitnet.Contracts.Core.TerminateContract;
+
+using DomainDrivenDesign.BuildingBlocks;
+
+public sealed record BindingContractTerminatedEvent(
+    Guid Id,
+    DateTimeOffset TerminatedAt,
+    DateTime OccuredAt) : IDomainEvent
+{
+    internal static BindingContractTerminatedEvent Create(DateTimeOffset terminatedAt)
+        => new(Guid.NewGuid(), terminatedAt, DateTime.Now);
+}
