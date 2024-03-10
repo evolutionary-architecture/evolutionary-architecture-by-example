49d298b5ab8f9e541b794742b811f81ad886cf46
Maciej Jedrzejewski <mac.jedrzejewski@gmail.com>
2024-03-10 14:35:27 +0100

    feat: add entity identifier

---
+++ Chapter-4-applying-tactical-domain-driven-design/Fitnet.Common/Fitnet.DomainDrivenDesign.BuildingBlocks/EntityIdentifier.cs
@@ -0,0 +1,54 @@
+namespace EvolutionaryArchitecture.Fitnet.DomainDrivenDesign.BuildingBlocks;
+
+using System;
+
+/// <summary>
+/// Base abstract class representing an entity identifier in the domain model.
+/// An entity identifier simplifies identification of objects.
+/// </summary>
+public abstract class EntityIdentifier<T> : IEquatable<EntityIdentifier<T>>
+{
+    public Guid Value { get; }
+
+    protected EntityIdentifier(Guid value)
+    {
+        if (value == Guid.Empty)
+        {
+            throw new ArgumentException($"{typeof(T).Name} cannot be empty.", nameof(value));
+        }
+
+        Value = value;
+    }
+
+    /// <summary>
+    /// Compares the current EntityIdentifier object with another object to determine if they are equal.
+    /// </summary>
+    public bool Equals(EntityIdentifier<T>? other) =>
+        other is not null && (ReferenceEquals(this, other) || Value.Equals(other.Value));
+
+    /// <summary>
+    /// Overrides the Equals method of the Object class
+    /// </summary>
+    public override bool Equals(object? obj) =>
+        ReferenceEquals(this, obj) || (obj is EntityIdentifier<T> other && Equals(other));
+
+    /// <summary>
+    /// Overrides the GetHashCode method to provide a hash code based on the Value property.
+    /// </summary>
+    public override int GetHashCode() => Value.GetHashCode();
+
+    /// <summary>
+    /// Overrides the equality operator == to provide equality comparison between EntityIdentifier objects.
+    /// </summary>
+    public static bool operator ==(EntityIdentifier<T> left, EntityIdentifier<T> right) => Equals(left, right);
+
+    /// <summary>
+    /// Overrides the equality operator != to provide equality comparison between EntityIdentifier objects.
+    /// </summary>
+    public static bool operator !=(EntityIdentifier<T> left, EntityIdentifier<T> right) => !Equals(left, right);
+
+    /// <summary>
+    /// Overrides the ToString method to return the string representation of the Value property.
+    /// </summary>
+    public override string ToString() => Value.ToString();
+}
---
+++ Chapter-4-applying-tactical-domain-driven-design/Fitnet.Common/Fitnet.DomainModel.UnitTests/EntityIdentifierTests.cs
@@ -0,0 +1,119 @@
+namespace EvolutionaryArchitecture.Fitnet.DomainModel.UnitTests;
+
+using DomainDrivenDesign.BuildingBlocks;
+
+public class EntityIdentifierTests
+{
+    [Fact]
+    internal void Given_valid_guid_Then_instance_should_be_successfully_created()
+    {
+        // Arrange
+        var validGuid = Guid.NewGuid();
+
+        // Act
+        EntityIdentifier<TestEntity> result = new TestEntity(validGuid);
+
+        // Assert
+        result.Value.Should().Be(validGuid);
+    }
+
+    [Fact]
+    internal void Given_empty_guid_Then_instance_creation_should_throw()
+    {
+        // Arrange
+        var emptyGuid = Guid.Empty;
+
+        // Act
+        var act = () => new TestEntity(emptyGuid);
+
+        // Assert
+        act.Should().Throw<ArgumentException>()
+            .WithMessage($"{nameof(TestEntity)} cannot be empty. (Parameter '{nameof(TestEntity.Value)}')");
+    }
+
+    [Fact]
+    internal void Given_two_entities_with_the_same_identifier_Then_equals_should_return_true()
+    {
+        // Arrange
+        var guid = Guid.NewGuid();
+        var entityIdentifier1 = new TestEntity(guid);
+        var entityIdentifier2 = new TestEntity(guid);
+
+        // Act
+        var result = entityIdentifier1.Equals(entityIdentifier2);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    internal void Given_two_entities_with_different_identifiers_Then_equals_should_return_false()
+    {
+        // Arrange
+        var entityIdentifier1 = new TestEntity(Guid.NewGuid());
+        var entityIdentifier2 = new TestEntity(Guid.NewGuid());
+
+        // Act
+        var result = entityIdentifier1.Equals(entityIdentifier2);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    internal void Given_entity_comparison_with_null_Then_equals_should_return_false()
+    {
+        // Arrange
+        var entityIdentifier = new TestEntity(Guid.NewGuid());
+
+        // Act
+        var result = entityIdentifier.Equals(null);
+
+        // Assert
+        Assert.False(result.Equals(null));
+    }
+
+    [Fact]
+    internal void Given_two_entities_with_the_same_identifier_Then_should_return_same_hash_codes()
+    {
+        // Arrange
+        var guid = Guid.NewGuid();
+        var entityIdentifier1 = new TestEntity(guid);
+        var entityIdentifier2 = new TestEntity(guid);
+
+        // Act
+        var firstHashcode = entityIdentifier1.GetHashCode();
+        var secondHashcode = entityIdentifier2.GetHashCode();
+
+        // Assert
+        firstHashcode.Should().Be(secondHashcode);
+    }
+
+    [Fact]
+    internal void Given_two_entities_with_different_identifiers_Then_should_return_different_hash_codes()
+    {
+        // Arrange
+        var entityIdentifier1 = new TestEntity(Guid.NewGuid());
+        var entityIdentifier2 = new TestEntity(Guid.NewGuid());
+
+        // Act
+        var firstHashcode = entityIdentifier1.GetHashCode();
+        var secondHashcode = entityIdentifier2.GetHashCode();
+
+        // Assert
+        firstHashcode.Should().NotBe(secondHashcode);
+    }
+
+    [Fact]
+    internal void Given_valid_entity_Then_should_return_guid_string()
+    {
+        // Arrange
+        var guid = Guid.NewGuid();
+        var entityIdentifier = new TestEntity(guid);
+
+        // Act & Assert
+        guid.ToString().Should().Be(entityIdentifier.ToString());
+    }
+}
+
+public class TestEntity(Guid value) : EntityIdentifier<TestEntity>(value);
