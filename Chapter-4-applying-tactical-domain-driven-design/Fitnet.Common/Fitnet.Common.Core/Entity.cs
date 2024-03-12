04933a97138573bc46b1c60b907880c5662135b2
Maciej Jedrzejewski <mac.jedrzejewski@gmail.com>
2024-03-12 09:04:39 +0100

    refactor: move entity and domain event interface to core project

--- Chapter-4-applying-tactical-domain-driven-design/Fitnet.Common/Fitnet.Common.Core/Entity.cs
+++ Chapter-4-applying-tactical-domain-driven-design/Fitnet.Common/Fitnet.Common.Core/Entity.cs
@@ -1,4 +1,4 @@
-﻿namespace EvolutionaryArchitecture.Fitnet.DomainDrivenDesign.BuildingBlocks;
+﻿namespace EvolutionaryArchitecture.Fitnet.Common.Core;
 
 /// <summary>
 /// Base abstract class representing an entity in the domain model.
