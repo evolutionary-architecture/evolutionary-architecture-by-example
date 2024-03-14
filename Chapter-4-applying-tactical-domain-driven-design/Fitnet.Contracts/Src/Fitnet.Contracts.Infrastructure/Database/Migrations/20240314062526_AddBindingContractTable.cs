b079a85caf591a021a5be94983ed2036d8cee5e9
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2024-03-14 07:33:57 +0100

    feat: add migration for binding contracts

---
+++ Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Infrastructure/Database/Migrations/20240314062526_AddBindingContractTable.cs
@@ -0,0 +1,33 @@
+﻿#nullable disable
+
+namespace EvolutionaryArchitecture.Fitnet.Contracts.Data.Database.Migrations;
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+/// <inheritdoc />
+public partial class AddBindingContractTable : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder) => migrationBuilder.CreateTable(
+            name: "BindingContracts",
+            schema: "Contracts",
+            columns: table => new
+            {
+                Id = table.Column<Guid>(type: "uuid", nullable: false),
+                ContractId = table.Column<Guid>(type: "uuid", nullable: false),
+                CustomerId = table.Column<Guid>(type: "uuid", nullable: false),
+                Duration = table.Column<TimeSpan>(type: "interval", nullable: false),
+                TerminatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                BindingFrom = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                ExpiringAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_BindingContracts", x => x.Id);
+            });
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder) => migrationBuilder.DropTable(
+            name: "BindingContracts",
+            schema: "Contracts");
+}
