41c3541643ac91b449eadcc4f35ef790ac2b26a3
Maciej Jedrzejewski <mac.jedrzejewski@gmail.com>
2024-03-25 15:46:09 +0100

    feat: add business rule for adding annex

---
+++ Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Core.UnitTests/AddAnnex/BusinessRules/AnnexCanOnlyBeAddedOnlyBeAddedToActiveBindingContractRuleTests.cs
@@ -0,0 +1,68 @@
+﻿namespace EvolutionaryArchitecture.Fitnet.Contracts.Core.UnitTests.AddAnnex.BusinessRules;
+
+using AddAnex.BusinessRules;
+using Common.Core.BusinessRules;
+
+public sealed class AnnexCanOnlyBeAddedOnlyBeAddedToActiveBindingContractRuleTests
+{
+    [Fact]
+    internal void Given_add_annex_When_binding_contract_terminated_Then_it_is_not_possible_to_add()
+    {
+        // Arrange
+        var now = DateTimeOffset.UtcNow;
+        var terminatedAt = now.AddDays(-1);
+        var expiringAt = now.AddYears(1);
+
+        // Act && Assert
+        ShouldThrowException(() =>
+            BusinessRuleValidator.Validate(
+                new AnnexCanOnlyBeAddedOnlyBeAddedToActiveBindingContractRule(terminatedAt, expiringAt)));
+    }
+
+    [Fact]
+    internal void Given_add_annex_When_binding_contract_expired_Then_it_is_not_possible_to_add()
+    {
+        // Arrange
+        var now = DateTimeOffset.UtcNow;
+        var expiringAt = now.AddDays(-1);
+
+        // Act && Assert
+        ShouldThrowException(() =>
+            BusinessRuleValidator.Validate(
+                new AnnexCanOnlyBeAddedOnlyBeAddedToActiveBindingContractRule(null, expiringAt)));
+    }
+
+    [Fact]
+    internal void Given_add_annex_When_binding_contract_expired_and_terminated_Then_it_is_not_possible_to_add()
+    {
+        // Arrange
+        var now = DateTimeOffset.UtcNow;
+        var terminatedAt = now.AddDays(-1);
+        var expiringAt = now.AddDays(-1);
+
+        // Act && Assert
+        ShouldThrowException(() =>
+            BusinessRuleValidator.Validate(
+                new AnnexCanOnlyBeAddedOnlyBeAddedToActiveBindingContractRule(terminatedAt, expiringAt)));
+    }
+
+    [Fact]
+    internal void Given_add_annex_When_binding_contract_is_not_expired_and_is_not_terminated_Then_it_is_possible_to_add()
+    {
+        // Arrange
+        var now = DateTimeOffset.UtcNow;
+        var expiringAt = now.AddYears(1);
+
+        // Act
+        var act = () =>
+            BusinessRuleValidator.Validate(
+                new AnnexCanOnlyBeAddedOnlyBeAddedToActiveBindingContractRule(null, expiringAt));
+
+        // Assert
+        act.Should().NotThrow();
+    }
+
+    private static void ShouldThrowException(Action act) =>
+        act.Should().Throw<BusinessRuleValidationException>()
+            .WithMessage("Annex can only be added to active binding contract");
+}
