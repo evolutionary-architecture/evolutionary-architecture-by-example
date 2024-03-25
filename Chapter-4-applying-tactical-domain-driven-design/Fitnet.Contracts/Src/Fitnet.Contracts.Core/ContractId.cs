f95ac52fa0f8c8a92e45b420752f14f0c51155a7
Maciej Jedrzejewski <mac.jedrzejewski@gmail.com>
2024-03-25 12:35:33 +0100

    feat: add anex id

--- Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Core/ContractId.cs
+++ Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Core/ContractId.cs
@@ -6,3 +6,8 @@ public readonly record struct BindingContractId(Guid Value)
 {
     internal static BindingContractId Create() => new(Guid.NewGuid());
 }
+
+public readonly record struct AnexId(Guid Value)
+{
+    internal static AnexId Create() => new(Guid.NewGuid());
+}
