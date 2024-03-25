558637b7032b78ce84313a11b2fa8f0df2f0af04
Maciej Jedrzejewski <mac.jedrzejewski@gmail.com>
2024-03-25 13:42:13 +0100

    feat: add scaffold for annex including adding logic

---
+++ Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Core/AddAnnex/AnnexAddedToBindingContractEvent.cs
@@ -0,0 +1,25 @@
+﻿namespace EvolutionaryArchitecture.Fitnet.Contracts.Core.AddAnex;
+
+using DomainDrivenDesign.BuildingBlocks;
+
+public sealed record AnnexAddedToBindingContractEvent(
+    Guid Id,
+    AnnexId AnnexId,
+    BindingContractId BindingContractId,
+    DateTimeOffset ValidFrom,
+    DateTimeOffset ExpiringAt,
+    DateTime OccuredAt) : IDomainEvent
+{
+    internal static AnnexAddedToBindingContractEvent Raise(
+        AnnexId annexId,
+        BindingContractId bindingContractId,
+        DateTimeOffset validFrom,
+        DateTimeOffset expiringAt)
+        => new(
+            Guid.NewGuid(),
+            annexId,
+            bindingContractId,
+            validFrom,
+            expiringAt,
+            DateTime.UtcNow);
+}
