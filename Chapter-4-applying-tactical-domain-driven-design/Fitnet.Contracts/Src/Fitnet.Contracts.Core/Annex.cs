fdb06f46643f163f50a4a49e76a897142cc5ef39
Maciej Jedrzejewski <mac.jedrzejewski@gmail.com>
2024-03-25 13:42:13 +0100

    feat: add scaffold for annex including adding logic

---
+++ Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Core/Annex.cs
@@ -0,0 +1,44 @@
+﻿namespace EvolutionaryArchitecture.Fitnet.Contracts.Core;
+
+using AddAnex;
+using DomainDrivenDesign.BuildingBlocks;
+
+public sealed class Annex : Entity
+{
+    public AnnexId Id { get; init; }
+    public BindingContractId BindingContractId { get; init; }
+    public TimeSpan Duration { get; init; }
+    public DateTimeOffset ValidFrom { get; init; }
+    public DateTimeOffset ExpiringAt { get; set; }
+
+    private Annex(
+        BindingContractId bindingContractId,
+        TimeSpan duration,
+        DateTimeOffset validFrom,
+        DateTimeOffset expiringAt)
+    {
+        Id = AnnexId.Create();
+        BindingContractId = bindingContractId;
+        Duration = duration;
+        ValidFrom = validFrom;
+        ExpiringAt = expiringAt;
+    }
+
+    internal static Annex Add(
+        BindingContractId bindingContractId,
+        TimeSpan duration,
+        DateTimeOffset validFrom,
+        DateTimeOffset expiringAt)
+    {
+        var annex = new Annex(bindingContractId, duration, validFrom, expiringAt);
+        var @event = AnnexAddedToBindingContractEvent.Raise(annex.Id, bindingContractId, validFrom, expiringAt);
+        annex.RecordEvent(@event);
+
+        return annex;
+    }
+}
+
+public readonly record struct AnnexId(Guid Value)
+{
+    internal static AnnexId Create() => new(Guid.NewGuid());
+}
