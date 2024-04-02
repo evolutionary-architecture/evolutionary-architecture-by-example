017dac6060eaee4dfca12c06f47f8e1249ce5103
Maciej Jedrzejewski <mac.jedrzejewski@gmail.com>
2024-04-02 18:04:01 +0200

    refactor: change the way how date time is handled in reports

--- Chapter-2-modules-separation/Src/Common/Fitnet.Common.IntegrationTests/GlobalUsings.cs
+++ Chapter-2-modules-separation/Src/Common/Fitnet.Common.IntegrationTests/GlobalUsings.cs
@@ -3,3 +3,6 @@ global using JetBrains.Annotations;
 global using Microsoft.AspNetCore.Mvc.Testing;
 global using Microsoft.AspNetCore.Hosting;
 global using Microsoft.Extensions.Configuration;
+global using Microsoft.AspNetCore.TestHost;
+global using Microsoft.Extensions.DependencyInjection;
+global using Bogus;
