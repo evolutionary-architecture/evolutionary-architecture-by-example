cdc15282a185743668106417cddb51c49a78f2e5
Maciej Jedrzejewski <mac.jedrzejewski@gmail.com>
2024-04-02 17:40:27 +0200

    refactor: remove not needed code

--- Chapter-1-initial-architecture/Src/Fitnet.IntegrationTests/Common/TestEngine/Time/FakeTimeProvider.cs
+++ Chapter-1-initial-architecture/Src/Fitnet.IntegrationTests/Common/TestEngine/Time/FakeTimeProvider.cs
@@ -8,7 +8,4 @@ public sealed class FakeTimeProvider : TimeProvider
     private DateTimeOffset TimeNowOffset { get; set; } = new Faker().Date.RecentOffset().UtcDateTime;
 
     public override DateTimeOffset GetUtcNow() => TimeNowOffset;
-
-    public void SimulateTimeSkip(int passedDays = 30) =>
-        TimeNowOffset = TimeNowOffset.AddDays(passedDays);
 }
