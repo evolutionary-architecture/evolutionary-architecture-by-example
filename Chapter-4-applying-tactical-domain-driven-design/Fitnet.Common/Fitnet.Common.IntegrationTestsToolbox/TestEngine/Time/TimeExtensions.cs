300792961acd69a7139ad68a87f889d8caa42d57
Maciej Jedrzejewski <mac.jedrzejewski@gmail.com>
2024-04-04 08:11:23 +0200

    refactor: rename time provider

--- Chapter-4-applying-tactical-domain-driven-design/Fitnet.Common/Fitnet.Common.IntegrationTestsToolbox/TestEngine/Time/TimeExtensions.cs
+++ Chapter-4-applying-tactical-domain-driven-design/Fitnet.Common/Fitnet.Common.IntegrationTestsToolbox/TestEngine/Time/TimeExtensions.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 public static class TimeExtensions
 {
     public static WebApplicationFactory<T> WithTime<T>(
-        this WebApplicationFactory<T> webApplicationFactory, FakeSystemTimeProvider fakeTimeProvider)
+        this WebApplicationFactory<T> webApplicationFactory, FakeTimeProvider fakeTimeProvider)
         where T : class => webApplicationFactory
         .WithWebHostBuilder(builder =>
             builder.ConfigureTestServices(services => services.AddSingleton<TimeProvider>(fakeTimeProvider)));
