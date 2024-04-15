1edc1a53f5bda6c7d85779678fa8878e62199d35
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2024-04-15 21:00:25 +0200

    feat: implement error or in app

--- Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Application/TerminateBindingContract/TerminateBindingContractCommandHandler.cs
+++ Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Application/TerminateBindingContract/TerminateBindingContractCommandHandler.cs
@@ -1,18 +1,21 @@
 namespace EvolutionaryArchitecture.Fitnet.Contracts.Application.TerminateBindingContract;
 
 using Common.Api.ErrorHandling;
+using ErrorOr;
 
 [UsedImplicitly]
 internal sealed class TerminateBindingContractCommandHandler(
     IBindingContractsRepository bindingContractsRepository,
-    TimeProvider timeProvider) : IRequestHandler<TerminateBindingContractCommand>
+    TimeProvider timeProvider) : IRequestHandler<TerminateBindingContractCommand, ErrorOr<Unit>>
 {
-    public async Task Handle(TerminateBindingContractCommand command, CancellationToken cancellationToken)
+    public async Task<ErrorOr<Unit>> Handle(TerminateBindingContractCommand command, CancellationToken cancellationToken)
     {
         var contract = await bindingContractsRepository.GetByIdAsync(command.BindingContractId, cancellationToken) ??
                        throw new ResourceNotFoundException(command.BindingContractId);
         var terminatedAt = timeProvider.GetUtcNow();
         contract.Terminate(terminatedAt);
         await bindingContractsRepository.CommitAsync(cancellationToken);
+
+        return Unit.Value;
     }
 }
