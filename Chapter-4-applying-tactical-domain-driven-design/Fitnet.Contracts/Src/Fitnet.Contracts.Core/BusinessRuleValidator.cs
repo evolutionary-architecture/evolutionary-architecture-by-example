f4f82a1d0600b94bafc3a94389b3eec2c0083b14
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2024-04-15 20:58:57 +0200

    feat: Add BusinessRuleValidator class and update Contract class to use ErrorOr type

---
+++ Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Core/BusinessRuleValidator.cs
@@ -0,0 +1,21 @@
+﻿namespace EvolutionaryArchitecture.Fitnet.Contracts.Core;
+
+using Common.Core.BusinessRules;
+using ErrorOr;
+
+public static class BusinessRuleValidator
+{
+    public static List<Error> Validate(params IBusinessRule[] rules)
+    {
+        var errors = new List<Error>();
+        foreach (var rule in rules)
+        {
+            if (!rule.IsMet())
+            {
+                errors.Add(Error.Validation(rule.Error));
+            }
+        }
+
+        return errors;
+    }
+}
