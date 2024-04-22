58ab174f83c28b639ae20006dc3bdffce64162f6
Maciej Jedrzejewski <mac.jedrzejewski@gmail.com>
2024-04-22 08:59:07 +0200

    feat: add command and handler for attach annex

---
+++ Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Application/AttachAnnex/AttachAnnexToBindingContractCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace EvolutionaryArchitecture.Fitnet.Contracts.Application.AttachAnnex;
+
+using Common.Api.ErrorHandling;
+
+[UsedImplicitly]
+internal sealed class AttachAnnexToBindingContractCommandHandler(
+    IBindingContractsRepository bindingContractsRepository) : IRequestHandler<AttachAnnexToBindingContractCommand>
+{
+    public async Task Handle(AttachAnnexToBindingContractCommand command, CancellationToken cancellationToken)
+    {
+        var contract =
+            await bindingContractsRepository.GetByContractIdAsync(command.BindingContractId, cancellationToken) ??
+            throw new ResourceNotFoundException(command.BindingContractId);
+        contract.AttachAnnex(command.ValidFrom);
+        await bindingContractsRepository.CommitAsync(cancellationToken);
+    }
+}
