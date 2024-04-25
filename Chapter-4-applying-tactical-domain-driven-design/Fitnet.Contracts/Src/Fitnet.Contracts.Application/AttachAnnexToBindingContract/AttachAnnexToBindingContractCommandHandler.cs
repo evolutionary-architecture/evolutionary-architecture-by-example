fb7c6afee5929c194e5b08547884061faef2ee4e
Maciej Jedrzejewski <mac.jedrzejewski@gmail.com>
2024-04-25 11:18:04 +0200

    refactor: add another business rule as it was suggested to attach annex

--- Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Application/AttachAnnexToBindingContract/AttachAnnexToBindingContractCommandHandler.cs
+++ Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Application/AttachAnnexToBindingContract/AttachAnnexToBindingContractCommandHandler.cs
@@ -4,14 +4,15 @@ using EvolutionaryArchitecture.Fitnet.Common.Api.ErrorHandling;
 
 [UsedImplicitly]
 internal sealed class AttachAnnexToBindingContractCommandHandler(
-    IBindingContractsRepository bindingContractsRepository) : IRequestHandler<AttachAnnexToBindingContractCommand>
+    IBindingContractsRepository bindingContractsRepository,
+    TimeProvider timeProvider) : IRequestHandler<AttachAnnexToBindingContractCommand>
 {
     public async Task Handle(AttachAnnexToBindingContractCommand command, CancellationToken cancellationToken)
     {
         var bindingContract =
             await bindingContractsRepository.GetByContractIdAsync(command.BindingContractId, cancellationToken) ??
             throw new ResourceNotFoundException(command.BindingContractId);
-        bindingContract.AttachAnnex(command.ValidFrom);
+        bindingContract.AttachAnnex(command.ValidFrom, timeProvider.GetUtcNow());
         await bindingContractsRepository.CommitAsync(cancellationToken);
     }
 }
