49650d5b0f1aec1efe7ef3798c34f3a5e66464a5
Maciej Jedrzejewski <mac.jedrzejewski@gmail.com>
2024-05-07 09:33:24 +0200

    refactor: change terminated at to nullable on binding contract

---
+++ Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Infrastructure/Database/Migrations/20240507073145_ChangeBindingContractTerminatedAtToNullable.cs
@@ -0,0 +1,31 @@
+﻿#nullable disable
+
+namespace EvolutionaryArchitecture.Fitnet.Contracts.Infrastructure.Database.Migrations;
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+/// <inheritdoc />
+public partial class ChangeBindingContractTerminatedAtToNullable : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder) => migrationBuilder.AlterColumn<DateTimeOffset>(
+            name: "TerminatedAt",
+            schema: "Contracts",
+            table: "BindingContracts",
+            type: "timestamp with time zone",
+            nullable: true,
+            oldClrType: typeof(DateTimeOffset),
+            oldType: "timestamp with time zone");
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder) => migrationBuilder.AlterColumn<DateTimeOffset>(
+            name: "TerminatedAt",
+            schema: "Contracts",
+            table: "BindingContracts",
+            type: "timestamp with time zone",
+            nullable: false,
+            defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)),
+            oldClrType: typeof(DateTimeOffset),
+            oldType: "timestamp with time zone",
+            oldNullable: true);
+}
