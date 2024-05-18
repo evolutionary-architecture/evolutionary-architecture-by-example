6dcfa83b090d4764a51d84c590fa5f8ff4500de8
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2024-05-18 19:28:17 +0200

    fix: add missing references

--- Chapter-2-modules-separation/Src/Common/Fitnet.Common.Infrastructure/CommonInfrastructureModule.cs
+++ Chapter-2-modules-separation/Src/Common/Fitnet.Common.Infrastructure/CommonInfrastructureModule.cs
@@ -3,14 +3,12 @@ namespace EvolutionaryArchitecture.Fitnet.Common.Infrastructure;
 using Clock;
 using Events.EventBus;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.FeatureManagement;
 
 public static class CommonInfrastructureModule
 {
     public static IServiceCollection AddCommonInfrastructure(this IServiceCollection services)
     {
         services.AddEventBus();
-        services.AddFeatureManagement();
         services.AddClock();
 
         return services;
