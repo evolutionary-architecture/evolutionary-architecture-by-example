151d4d2aad33b1afc9d436e5f899ccface0a99ae
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2024-05-18 19:28:17 +0200

    fix: add missing references

--- Chapter-2-modules-separation/Src/Common/Fitnet.Common.Infrastructure/Modules/ModuleAvailabilityChecker.cs
+++ Chapter-2-modules-separation/Src/Common/Fitnet.Common.Infrastructure/Modules/ModuleAvailabilityChecker.cs
@@ -7,7 +7,9 @@ public static class ModuleAvailabilityChecker
     private const string EnabilityConfigName = "Enabled";
 
     public static bool IsModuleEnabled(this IConfiguration configuration, string module) =>
-        configuration.GetSection(GetModuleConfiguration(module)).GetValue<bool>(EnabilityConfigName);
+        configuration.GetSection(
+                GetModuleConfiguration(module))
+            .GetValue<bool>(EnabilityConfigName);
 
     private static string GetModuleConfiguration(string module) => $"Modules:{module}";
 }
