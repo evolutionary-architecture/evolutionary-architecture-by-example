7a0b0ab1098d95d2c07e2c6a3032c622950ee2b9
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2024-05-19 19:45:57 +0200

    styles: fixes in formatting

--- Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Api/AttachAnnexToBindingContract/AttachAnnexToBindingContractEndpoint.cs
+++ Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Api/AttachAnnexToBindingContract/AttachAnnexToBindingContractEndpoint.cs
@@ -8,24 +8,25 @@ using Microsoft.AspNetCore.Routing;
 
 internal static class AttachAnnexToBindingContractEndpoint
 {
-    internal static void MapAttachAnnexToBindingContract(this IEndpointRouteBuilder app) => app.MapPost(
-            ContractsApiPaths.AttachAnnex, async (
-                    Guid id,
-                    AttachAnnexToBindingContractRequest request,
-                    IContractsModule contractsModule,
-                    CancellationToken cancellationToken) =>
-                await contractsModule.ExecuteCommandAsync(request.ToCommand(id), cancellationToken)
-                    .Match(annexId => Results.Created(BuildUrl(id, annexId), annexId),
-                        errors => errors.ToProblem()))
-        .WithOpenApi(operation => new(operation)
-        {
-            Summary = "Attach annex to existing binding contract",
-            Description = "This endpoint is used to attach an annex to an existing binding contract.",
-        })
-        .Produces<string>(StatusCodes.Status201Created)
-        .Produces(StatusCodes.Status404NotFound)
-        .Produces(StatusCodes.Status409Conflict)
-        .Produces(StatusCodes.Status500InternalServerError);
+    internal static void MapAttachAnnexToBindingContract(this IEndpointRouteBuilder app) =>
+        app.MapPost(
+                ContractsApiPaths.AttachAnnex, async (
+                        Guid id,
+                        AttachAnnexToBindingContractRequest request,
+                        IContractsModule contractsModule,
+                        CancellationToken cancellationToken) =>
+                    await contractsModule.ExecuteCommandAsync(request.ToCommand(id), cancellationToken)
+                        .Match(annexId => Results.Created(BuildUrl(id, annexId), annexId),
+                            errors => errors.ToProblem()))
+            .WithOpenApi(operation => new(operation)
+            {
+                Summary = "Attach annex to existing binding contract",
+                Description = "This endpoint is used to attach an annex to an existing binding contract.",
+            })
+            .Produces<string>(StatusCodes.Status201Created)
+            .Produces(StatusCodes.Status404NotFound)
+            .Produces(StatusCodes.Status409Conflict)
+            .Produces(StatusCodes.Status500InternalServerError);
 
     private static string BuildUrl(Guid bindingContractId, Guid annexId)
     {
