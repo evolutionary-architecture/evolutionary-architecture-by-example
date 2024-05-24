e8f636c246dc5df6a3f78c0633ce6ce05a0a3021
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2024-05-24 22:06:44 +0200

    restore: unessery changes

--- Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Api/PrepareContract/PrepareContractEndpoint.cs
+++ Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Api/PrepareContract/PrepareContractEndpoint.cs
@@ -2,21 +2,19 @@ namespace EvolutionaryArchitecture.Fitnet.Contracts.Api.PrepareContract;
 
 using EvolutionaryArchitecture.Fitnet.Common.Api.Validations;
 using Application;
+using Common.Errors;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 
 internal static class PrepareContractEndpoint
 {
-    internal static void MapPrepareContract(this IEndpointRouteBuilder app) => app.MapPost(ContractsApiPaths.Prepare,
-            async (PrepareContractRequest request, IContractsModule contractsModule,
-                CancellationToken cancellationToken) =>
-            {
-                var command = request.ToCommand();
-                var contractId = await contractsModule.ExecuteCommandAsync(command, cancellationToken);
-
-                return Results.Created(ContractsApiPaths.GetPreparedContractPath(contractId), contractId);
-            })
+    internal static void MapPrepareContract(this IEndpointRouteBuilder app) =>
+        app.MapPost(ContractsApiPaths.Prepare,
+            async Task (PrepareContractRequest request, IContractsModule contractsModule, CancellationToken cancellationToken) =>
+                await contractsModule.ExecuteCommandAsync(request.ToCommand(), cancellationToken).Match(
+                contractId => Results.Created(ContractsApiPaths.GetPreparedContractPath(contractId), (object?)contractId),
+                errors => errors.ToProblem()))
         .ValidateRequest<PrepareContractRequest>()
         .WithOpenApi(operation => new(operation)
         {
