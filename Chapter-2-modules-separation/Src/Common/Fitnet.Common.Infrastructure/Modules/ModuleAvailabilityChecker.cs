cc145ea7f4c9df8200b5bd3faa352a374c7a694a
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2024-05-27 17:23:02 +0200

    refactor: rename EnabilityConfigName

--- Chapter-2-modules-separation/Src/Common/Fitnet.Common.Infrastructure/Modules/ModuleAvailabilityChecker.cs
+++ Chapter-2-modules-separation/Src/Common/Fitnet.Common.Infrastructure/Modules/ModuleAvailabilityChecker.cs
@@ -4,12 +4,12 @@ using Microsoft.Extensions.Configuration;
 
 public static class ModuleAvailabilityChecker
 {
-    private const string EnabilityConfigName = "Enabled";
+    private const string AvailabilityConfigKeyName = "Enabled";
 
     public static bool IsModuleEnabled(this IConfiguration configuration, string module) =>
         configuration.GetSection(
                 GetModuleConfiguration(module))
-            .GetValue<bool>(EnabilityConfigName);
+            .GetValue<bool>(AvailabilityConfigKeyName);
 
     private static string GetModuleConfiguration(string module) => $"Modules:{module}";
 }
