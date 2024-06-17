77ee25a701bcfa19a127214ccc83e3c6fe77e6f3
Maciej Jedrzejewski <mac.jedrzejewski@gmail.com>
2024-06-17 14:28:55 +0200

    feat: add value object into package

---
+++ Chapter-4-applying-tactical-domain-driven-design/Fitnet.Common/Fitnet.Common.Core/ValueObject.cs
@@ -0,0 +1,26 @@
+﻿namespace EvolutionaryArchitecture.Fitnet.Common.Core;
+
+public abstract class ValueObject
+{
+    protected abstract IEnumerable<object> GetEqualityComponents();
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null || obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        var other = (ValueObject)obj;
+
+        return GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+    }
+
+    public override int GetHashCode() => GetEqualityComponents()
+            .Aggregate(0, (current, obj) => current ^ (obj?.GetHashCode() ?? 0));
+
+    public static bool operator ==(ValueObject left, ValueObject right) => left.Equals(right);
+
+    public static bool operator !=(ValueObject left, ValueObject right) => !(left == right);
+}
+
