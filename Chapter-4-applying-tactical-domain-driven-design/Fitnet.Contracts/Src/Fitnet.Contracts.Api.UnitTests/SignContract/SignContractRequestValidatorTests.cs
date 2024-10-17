fce85877cee22e198d552c688f13018f029ceaf0
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2024-10-17 17:19:23 +0200

    feat: add unit test that covers to long text

--- Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Api.UnitTests/SignContract/SignContractRequestValidatorTests.cs
+++ Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Api.UnitTests/SignContract/SignContractRequestValidatorTests.cs
@@ -6,6 +6,7 @@ using FluentValidation.TestHelper;
 public sealed class SignContractRequestValidatorTests
 {
     private const string ValidSignatureText = "John Doe";
+    private const int SignatureCharacterLimit = 100;
     private readonly SignContractRequestValidator _validator = new();
     private readonly DateTimeOffset _fakeNow = new Faker().Date.RecentOffset();
 
@@ -34,4 +35,21 @@ public sealed class SignContractRequestValidatorTests
         // Assert
         result.ShouldHaveValidationErrorFor(signContractRequest => signContractRequest.SignedAt);
     }
+
+
+    [Fact]
+    internal void Given_sign_contract_request_validation_When_signature_text_is_to_long_Then_result_should_have_error()
+    {
+        // Arrange
+        var tooLongSignatureText = GenerateTooLongSignatureText();
+        var request = new SignContractRequest(default, tooLongSignatureText);
+
+        // Act
+        var result = _validator.TestValidate(request);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(signContractRequest => signContractRequest.SignedAt);
+    }
+
+    private static string GenerateTooLongSignatureText() => new('a', SignatureCharacterLimit + 1);
 }
