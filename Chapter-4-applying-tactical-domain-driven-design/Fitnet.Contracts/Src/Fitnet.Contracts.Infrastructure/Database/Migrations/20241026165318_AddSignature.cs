907d1339ef5526d6acdef3126530e9b00a5a95f6
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2024-10-26 18:54:41 +0200

    feat: fix migration

--- Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Infrastructure/Database/Migrations/20241026165318_AddSignature.cs
+++ Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Infrastructure/Database/Migrations/20241026165318_AddSignature.cs
@@ -1,43 +1,41 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿#nullable disable
 
-#nullable disable
+namespace EvolutionaryArchitecture.Fitnet.Contracts.Infrastructure.Database.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace EvolutionaryArchitecture.Fitnet.Contracts.Infrastructure.Database.Migrations
+/// <inheritdoc />
+public partial class AddSignature : Migration
 {
     /// <inheritdoc />
-    public partial class AddSignature : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.RenameColumn(
-                name: "SignedAt",
-                schema: "Contracts",
-                table: "Contracts",
-                newName: "Signature_Date");
+        migrationBuilder.RenameColumn(
+            name: "SignedAt",
+            schema: "Contracts",
+            table: "Contracts",
+            newName: "Signature_Date");
 
-            migrationBuilder.AddColumn<string>(
-                name: "Signature_Value",
-                schema: "Contracts",
-                table: "Contracts",
-                type: "character varying(100)",
-                maxLength: 100,
-                nullable: true);
-        }
+        migrationBuilder.AddColumn<string>(
+            name: "Signature_Value",
+            schema: "Contracts",
+            table: "Contracts",
+            type: "character varying(100)",
+            maxLength: 100,
+            nullable: true);
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "Signature_Value",
-                schema: "Contracts",
-                table: "Contracts");
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "Signature_Value",
+            schema: "Contracts",
+            table: "Contracts");
 
-            migrationBuilder.RenameColumn(
-                name: "Signature_Date",
-                schema: "Contracts",
-                table: "Contracts",
-                newName: "SignedAt");
-        }
+        migrationBuilder.RenameColumn(
+            name: "Signature_Date",
+            schema: "Contracts",
+            table: "Contracts",
+            newName: "SignedAt");
     }
 }
