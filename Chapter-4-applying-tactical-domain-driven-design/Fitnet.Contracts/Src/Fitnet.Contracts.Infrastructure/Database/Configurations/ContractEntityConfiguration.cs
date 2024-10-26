a2b3f76eef3c6fd39cc277c3a6e50d57cd7ddf3b
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2024-10-26 19:01:28 +0200

    Update ContractEntityConfiguration.cs

--- Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Infrastructure/Database/Configurations/ContractEntityConfiguration.cs
+++ Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Infrastructure/Database/Configurations/ContractEntityConfiguration.cs
@@ -3,7 +3,6 @@ namespace EvolutionaryArchitecture.Fitnet.Contracts.Infrastructure.Database.Conf
 using Core;
 using Core.SignContract.Signatures;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 internal sealed class ContractEntityConfiguration : IEntityTypeConfiguration<Contract>
@@ -24,16 +23,5 @@ internal sealed class ContractEntityConfiguration : IEntityTypeConfiguration<Con
             signatureBuilder.Property(signature => signature.Date).IsRequired();
             signatureBuilder.Property(signature => signature.Value).IsRequired().HasMaxLength(100);
         });
-    }
-}
-
-public class BloggingContextFactory : IDesignTimeDbContextFactory<ContractsPersistence>
-{
-    public ContractsPersistence CreateDbContext(string[] args)
-    {
-        var optionsBuilder = new DbContextOptionsBuilder<ContractsPersistence>();
-        optionsBuilder.UseNpgsql("Host=localhost:5432;Database=fitnet;Username=postgres;Password=mysecretpassword");
-
-        return new ContractsPersistence(optionsBuilder.Options);
     }
 }
