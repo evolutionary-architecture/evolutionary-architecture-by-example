25127cac07ad8d8f9e7e71478a827df53d240ea0
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2024-10-27 14:43:10 +0100

    fix: formatting

--- Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Application/TerminateBindingContract/TerminateBindingContractCommandHandler.cs
+++ Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Application/TerminateBindingContract/TerminateBindingContractCommandHandler.cs
@@ -14,7 +14,7 @@ internal sealed class TerminateBindingContractCommandHandler(
                 .ThenAsync(async _ =>
                 {
                     await bindingContractsRepository.CommitAsync(cancellationToken);
-                    
+
                     return Unit.Value;
                 }));
 }
