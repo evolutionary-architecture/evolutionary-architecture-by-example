8c3adc73daaf65596688a0b20b0e60b9652aa4d7
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2025-01-02 20:53:34 +0100

    feat: upgrade test to .NET 9

--- Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Api.UnitTests/PrepareContract/RequestValidator/InvalidPrepareContractRequestTestCases.cs
+++ Chapter-4-applying-tactical-domain-driven-design/Fitnet.Contracts/Src/Fitnet.Contracts.Api.UnitTests/PrepareContract/RequestValidator/InvalidPrepareContractRequestTestCases.cs
@@ -1,33 +1,48 @@
 namespace EvolutionaryArchitecture.Fitnet.Contracts.Api.UnitTests.PrepareContract.RequestValidator;
 
+using System.Collections;
 using EvolutionaryArchitecture.Fitnet.Contracts.Api.PrepareContract;
 
-internal sealed class InvalidPrepareContractRequestTestCases : TheoryData<PrepareContractRequest>
+internal sealed class InvalidPrepareContractRequestTestCases : IEnumerable<object[]>
 {
+    private readonly List<object[]> _testCases = [];
     private readonly Faker _faker = new();
     private readonly DateTimeOffset _fakeNow = new Faker().Date.RecentOffset();
 
     public InvalidPrepareContractRequestTestCases()
     {
         var validContractParameters = PrepareContractRequestParameters.GetValid();
-        AddRow(
-            new PrepareContractRequest(Guid.Empty, validContractParameters.MinAge, validContractParameters.MaxHeight,
-                _fakeNow), nameof(PrepareContractRequest.CustomerId));
-        AddRow(new PrepareContractRequest(Guid.NewGuid(), default, validContractParameters.MaxHeight, _fakeNow),
-            nameof(PrepareContractRequest.CustomerAge));
-        AddRow(
-            new PrepareContractRequest(Guid.NewGuid(), _faker.Random.Number(-100, -1),
-                validContractParameters.MaxHeight, _fakeNow), nameof(PrepareContractRequest.CustomerAge));
-        AddRow(new PrepareContractRequest(Guid.NewGuid(), validContractParameters.MinAge, default, _fakeNow),
-            nameof(PrepareContractRequest.CustomerHeight));
-        AddRow(
-            new PrepareContractRequest(Guid.NewGuid(), validContractParameters.MinAge, _faker.Random.Number(-100, -1),
-                _fakeNow), nameof(PrepareContractRequest.CustomerHeight));
-        AddRow(
-            new PrepareContractRequest(Guid.NewGuid(), validContractParameters.MinAge,
-                validContractParameters.MaxHeight, default), nameof(PrepareContractRequest.PreparedAt));
-        AddRow(
-            new PrepareContractRequest(Guid.NewGuid(), validContractParameters.MinAge,
-                validContractParameters.MaxHeight, default), nameof(PrepareContractRequest.PreparedAt));
+        _testCases.Add([
+            new PrepareContractRequest(Guid.Empty, validContractParameters.MinAge, validContractParameters.MaxHeight, _fakeNow),
+            nameof(PrepareContractRequest.CustomerId)
+        ]);
+        _testCases.Add([
+            new PrepareContractRequest(Guid.NewGuid(), default, validContractParameters.MaxHeight, _fakeNow),
+            nameof(PrepareContractRequest.CustomerAge)
+        ]);
+        _testCases.Add([
+            new PrepareContractRequest(Guid.NewGuid(), _faker.Random.Number(-100, -1), validContractParameters.MaxHeight, _fakeNow),
+            nameof(PrepareContractRequest.CustomerAge)
+        ]);
+        _testCases.Add([
+            new PrepareContractRequest(Guid.NewGuid(), validContractParameters.MinAge, default, _fakeNow),
+            nameof(PrepareContractRequest.CustomerHeight)
+        ]);
+        _testCases.Add([
+            new PrepareContractRequest(Guid.NewGuid(), validContractParameters.MinAge, _faker.Random.Number(-100, -1), _fakeNow),
+            nameof(PrepareContractRequest.CustomerHeight)
+        ]);
+        _testCases.Add([
+            new PrepareContractRequest(Guid.NewGuid(), validContractParameters.MinAge, validContractParameters.MaxHeight, default),
+            nameof(PrepareContractRequest.PreparedAt)
+        ]);
+        _testCases.Add([
+            new PrepareContractRequest(Guid.NewGuid(), validContractParameters.MinAge, validContractParameters.MaxHeight, default),
+            nameof(PrepareContractRequest.PreparedAt)
+        ]);
     }
+
+    public IEnumerator<object[]> GetEnumerator() => _testCases.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }
