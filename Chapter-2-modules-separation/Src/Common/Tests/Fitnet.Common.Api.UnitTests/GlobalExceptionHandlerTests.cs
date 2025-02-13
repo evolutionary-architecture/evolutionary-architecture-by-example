09fd197e22c482bf226f39a839f2f83dcaba20cc
kamilbaczek <kaba@softwaremind.com>
2025-02-13 22:24:35 +0100

    refactor: add Shouldly global usings and remove unused references in tests

--- Chapter-2-modules-separation/Src/Common/Tests/Fitnet.Common.Api.UnitTests/GlobalExceptionHandlerTests.cs
+++ Chapter-2-modules-separation/Src/Common/Tests/Fitnet.Common.Api.UnitTests/GlobalExceptionHandlerTests.cs
@@ -4,7 +4,6 @@ using ErrorHandling;
 using Core.BusinessRules;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Shouldly;
 
 public sealed class GlobalExceptionHandlerTests
 {
