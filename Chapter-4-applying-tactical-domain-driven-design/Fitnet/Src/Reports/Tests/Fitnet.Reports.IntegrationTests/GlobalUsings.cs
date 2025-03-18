90a11764105f07d6de7a5cc10ded19ea27146f12
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2025-03-18 18:16:19 +0100

    feat: add Shouldly package and update assertions in integration tests

--- Chapter-4-applying-tactical-domain-driven-design/Fitnet/Src/Reports/Tests/Fitnet.Reports.IntegrationTests/GlobalUsings.cs
+++ Chapter-4-applying-tactical-domain-driven-design/Fitnet/Src/Reports/Tests/Fitnet.Reports.IntegrationTests/GlobalUsings.cs
@@ -1,6 +1,5 @@
 global using System.Net;
 global using System.Net.Http.Json;
-global using Xunit;
 global using Bogus;
-global using FluentAssertions;
 global using MassTransit.Testing;
+global using Shouldly;
