eea4bd878e0b51adb1181b9a5c190ce3c1effe8f
Kamil Bączek <kamilbaczek98@gmail.com>
2025-04-30 23:34:15 +0200

    feat: update docker-compose with database connection strings and enhance API documentation endpoint

--- Chapter-1-initial-architecture/Src/Fitnet/Common/Documentation/SwaggerDocumentationExtensions.cs
+++ Chapter-1-initial-architecture/Src/Fitnet/Common/Documentation/SwaggerDocumentationExtensions.cs
@@ -1,7 +1,16 @@
 ﻿namespace EvolutionaryArchitecture.Fitnet.Common.Documentation;
 
+using Microsoft.OpenApi.Models;
+
 internal static class ApiDocumentationExtensions
 {
     internal static void UseApiDocumentation(this IEndpointRouteBuilder app) =>
-        app.MapGet("/", () => Results.Redirect("/swagger"));
+        app.MapGet("/", () => Results.Redirect("/swagger"))
+            .WithOpenApi(operation => new OpenApiOperation(operation)
+            {
+                Summary = "Documentation for the API",
+                Description = "This endpoint is used to redirect to the documentation for the API.",
+            })
+            .Produces(StatusCodes.Status200OK)
+            .WithTags("Documentation");
 }
