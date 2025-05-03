cd2d3766ac2922b03bb4d9466766a5846ce8490e
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2025-05-03 13:07:16 +0200

    fix: correct indentation in UseApiDocumentation method for consistency

--- Chapter-2-modules-separation/Src/Common/Fitnet.Common.Api/Documentation/SwaggerDocumentationExtensions.cs
+++ Chapter-2-modules-separation/Src/Common/Fitnet.Common.Api/Documentation/SwaggerDocumentationExtensions.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Routing;
 
 public static class ApiDocumentationExtensions
 {
- public static void UseApiDocumentation(this IEndpointRouteBuilder app) =>
+    public static void UseApiDocumentation(this IEndpointRouteBuilder app) =>
         app.MapGet("/", () => Results.Redirect("/swagger"))
             .Produces(StatusCodes.Status200OK)
             .WithTags("Documentation");
