c81e0bde971032b304bbde699888fadc2cf80289
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2025-05-03 13:06:44 +0200

    feat: add API documentation extension for Swagger redirection

---
+++ Chapter-2-modules-separation/Src/Common/Fitnet.Common.Api/Documentation/SwaggerDocumentationExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace EvolutionaryArchitecture.Fitnet.Common.Api.Documentation;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+public static class ApiDocumentationExtensions
+{
+ public static void UseApiDocumentation(this IEndpointRouteBuilder app) =>
+        app.MapGet("/", () => Results.Redirect("/swagger"))
+            .Produces(StatusCodes.Status200OK)
+            .WithTags("Documentation");
+}
