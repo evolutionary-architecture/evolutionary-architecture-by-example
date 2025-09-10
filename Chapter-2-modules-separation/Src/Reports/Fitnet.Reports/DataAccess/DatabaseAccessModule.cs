1b13b1a3ae0d2a8346993617cc31b191c61020d4
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2025-09-10 20:20:21 +0200

    feat(db): implement options pattern for database connection strings chapter 2

--- Chapter-2-modules-separation/Src/Reports/Fitnet.Reports/DataAccess/DatabaseAccessModule.cs
+++ Chapter-2-modules-separation/Src/Reports/Fitnet.Reports/DataAccess/DatabaseAccessModule.cs
@@ -1,11 +1,15 @@
 namespace EvolutionaryArchitecture.Fitnet.Reports.DataAccess;
 
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 internal static class DatabaseAccessModule
 {
-    internal static IServiceCollection AddDataAccess(this IServiceCollection services)
+    internal static IServiceCollection AddDataAccess(this IServiceCollection services, IConfiguration configuration)
     {
+        services.Configure<ReportsPersistenceOptions>(configuration.GetSection(ReportsPersistenceOptions.SectionName));
+        services.AddOptionsWithValidateOnStart<ReportsPersistenceOptions>();
+
         services.AddSingleton<IDatabaseConnectionFactory, DatabaseConnectionFactory>();
 
         return services;
