93e960ea3e1444f5dcd8aaa513d2e7e1ad1b9d6f
kamilbaczek <kamilbaczek98@gmail.com>
2025-12-15 20:56:50 +0100

    fix: fixed connection strings

--- Chapter-2-modules-separation/Src/Fitnet.AppHost/Program.cs
+++ Chapter-2-modules-separation/Src/Fitnet.AppHost/Program.cs
@@ -4,14 +4,14 @@ var builder = DistributedApplication.CreateBuilder(args);
 var postgres = builder.AddPostgres("postgres")
     .WithImage("postgres", "14.3")
     .WithPgAdmin();
+var db = postgres.AddDatabase("fitnetsdb", "fitnet");
 
-var fitnetDatabase = postgres.AddDatabase("fitnetsdb", "fitnet");
 builder.AddProject<Fitnet>("fitnet")
     .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development")
-    .WithReference(fitnetDatabase, "Modules__Passes__ConnectionStrings__Primary")
-    .WithReference(fitnetDatabase, "Modules__Contracts__ConnectionStrings__Primary")
-    .WithReference(fitnetDatabase, "Modules__Reports__ConnectionStrings__Primary")
-    .WithReference(fitnetDatabase, "Modules__Offers__ConnectionStrings__Primary")
+    .WithEnvironment("Passes__ConnectionStrings__Primary", db)
+    .WithEnvironment("Contracts__ConnectionStrings__Primary", db)
+    .WithEnvironment("Reports__ConnectionStrings__Primary", db)
+    .WithEnvironment("Offers__ConnectionStrings__Primary", db)
     .WaitFor(postgres);
 
 await builder.Build().RunAsync();
