7ff5bc965a225d9fad7134d2f2625125df3bc5d7
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2026-01-24 09:11:22 +0100

    feat: upgrade .net to 10

--- Chapter-1-initial-architecture/Src/Fitnet.UnitTests/Contracts/PrepareContract/RequestValidator/InvalidPrepareContractRequestTestCases.cs
+++ Chapter-1-initial-architecture/Src/Fitnet.UnitTests/Contracts/PrepareContract/RequestValidator/InvalidPrepareContractRequestTestCases.cs
@@ -2,23 +2,21 @@ namespace EvolutionaryArchitecture.Fitnet.UnitTests.Contracts.PrepareContract.Re
 
 using EvolutionaryArchitecture.Fitnet.Contracts.PrepareContract;
 
-internal sealed class InvalidPrepareContractRequestTestCases : IEnumerable<object[]>
+internal sealed class InvalidPrepareContractRequestTestCases : TheoryData<PrepareContractRequest, string>
 {
     private readonly Faker _faker = new();
     private readonly DateTimeOffset _fakeNow = new Faker().Date.RecentOffset();
 
-    public IEnumerator<object[]> GetEnumerator()
+    public InvalidPrepareContractRequestTestCases()
     {
         var validContractParameters = PrepareContractParameters.GetValid();
 
-        yield return new object[] { new PrepareContractRequest(Guid.Empty, validContractParameters.MinAge, validContractParameters.MaxHeight, _fakeNow), nameof(PrepareContractRequest.CustomerId) };
-        yield return new object[] { new PrepareContractRequest(Guid.NewGuid(), default, validContractParameters.MaxHeight, _fakeNow), nameof(PrepareContractRequest.CustomerAge) };
-        yield return new object[] { new PrepareContractRequest(Guid.NewGuid(), _faker.Random.Number(-100, -1), validContractParameters.MaxHeight, _fakeNow), nameof(PrepareContractRequest.CustomerAge) };
-        yield return new object[] { new PrepareContractRequest(Guid.NewGuid(), validContractParameters.MinAge, default, _fakeNow), nameof(PrepareContractRequest.CustomerHeight) };
-        yield return new object[] { new PrepareContractRequest(Guid.NewGuid(), validContractParameters.MinAge, _faker.Random.Number(-100, -1), _fakeNow), nameof(PrepareContractRequest.CustomerHeight) };
-        yield return new object[] { new PrepareContractRequest(Guid.NewGuid(), validContractParameters.MinAge, validContractParameters.MaxHeight, default), nameof(PrepareContractRequest.PreparedAt) };
-        yield return new object[] { new PrepareContractRequest(Guid.NewGuid(), validContractParameters.MinAge, validContractParameters.MaxHeight, default), nameof(PrepareContractRequest.PreparedAt) };
+        Add(new PrepareContractRequest(Guid.Empty, validContractParameters.MinAge, validContractParameters.MaxHeight, _fakeNow), nameof(PrepareContractRequest.CustomerId));
+        Add(new PrepareContractRequest(Guid.NewGuid(), default, validContractParameters.MaxHeight, _fakeNow), nameof(PrepareContractRequest.CustomerAge));
+        Add(new PrepareContractRequest(Guid.NewGuid(), _faker.Random.Number(-100, -1), validContractParameters.MaxHeight, _fakeNow), nameof(PrepareContractRequest.CustomerAge));
+        Add(new PrepareContractRequest(Guid.NewGuid(), validContractParameters.MinAge, default, _fakeNow), nameof(PrepareContractRequest.CustomerHeight));
+        Add(new PrepareContractRequest(Guid.NewGuid(), validContractParameters.MinAge, _faker.Random.Number(-100, -1), _fakeNow), nameof(PrepareContractRequest.CustomerHeight));
+        Add(new PrepareContractRequest(Guid.NewGuid(), validContractParameters.MinAge, validContractParameters.MaxHeight, default), nameof(PrepareContractRequest.PreparedAt));
+        Add(new PrepareContractRequest(Guid.NewGuid(), validContractParameters.MinAge, validContractParameters.MaxHeight, default), nameof(PrepareContractRequest.PreparedAt));
     }
-
-    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }
